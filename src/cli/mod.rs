b791e3de69ec85d202db6c6b046b60fb3228c59f
Ruben Arts <ruben.arts@hotmail.com>
2023-05-04 17:08:09 +0200

    add completion command

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,5 +1,7 @@
-use clap::Parser;
+use clap::{CommandFactory, Parser};
+use clap_complete::Shell;
 
+use anyhow::Error;
 mod add;
 mod init;
 mod run;
@@ -11,18 +13,42 @@ struct Args {
     command: Command,
 }
 
+/// Generates a completion script for a shell.
+#[derive(Parser, Debug)]
+pub struct CompletionCommand {
+    /// The shell to generate a completion script for (defaults to 'bash').
+    #[arg(short, long)]
+    shell: Option<Shell>,
+}
+
+
+
 #[derive(Parser, Debug)]
 enum Command {
+    Completion(CompletionCommand),
     Init(init::Args),
     Add(add::Args),
     Sync(sync::Args),
     Run(run::Args),
 }
 
+
+fn completion(args: CompletionCommand) -> Result<(), Error> {
+    clap_complete::generate(
+        args.shell.unwrap_or(Shell::Bash),
+        &mut Args::command(),
+        "pax",
+        &mut std::io::stdout(),
+    );
+
+    Ok(())
+}
+
 pub async fn execute() -> anyhow::Result<()> {
     let args = Args::parse();
 
     match args.command {
+        Command::Completion(cmd) => completion(cmd),
         Command::Init(cmd) => init::execute(cmd).await,
         Command::Add(cmd) => add::execute(cmd).await,
         Command::Sync(cmd) => sync::execute(cmd).await,
