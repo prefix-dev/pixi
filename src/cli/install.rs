3ca09c0b9770f601f6558ef7d9fa5353dcbdf153
Bas Zalmstra <bas@prefix.dev>
2023-06-02 13:24:58 +0200

    feat: install on windows, add virtual packages when installing (#47)

--- src/cli/install.rs
+++ src/cli/install.rs
@@ -1,22 +1,31 @@
-use crate::environment::execute_transaction;
-use crate::prefix::Prefix;
-use crate::progress::await_in_progress;
-use crate::repodata::{fetch_sparse_repodata, friendly_channel_name};
+use crate::repodata::friendly_channel_name;
+use crate::{
+    environment::execute_transaction, prefix::Prefix, progress::await_in_progress,
+    repodata::fetch_sparse_repodata,
+};
 use clap::Parser;
 use dirs::home_dir;
+use itertools::Itertools;
 use rattler::install::Transaction;
 use rattler_conda_types::{Channel, ChannelConfig, MatchSpec, Platform, PrefixRecord};
 use rattler_repodata_gateway::sparse::SparseRepoData;
-use rattler_shell::activation::{ActivationVariables, Activator};
+use rattler_shell::{
+    activation::{ActivationVariables, Activator},
+    shell::Shell,
+    shell::ShellEnum,
+};
 use rattler_solve::{LibsolvRepoData, SolverBackend};
 use reqwest::Client;
-use std::ops::Add;
-use std::path::PathBuf;
-use std::str::FromStr;
+use std::{
+    path::{Path, PathBuf},
+    str::FromStr,
+};
+
+const BIN_DIR: &str = ".pax/bin";
+const BIN_ENVS_DIR: &str = ".pax/envs";
 
 /// Runs command in project.
 #[derive(Parser, Debug)]
-#[clap(trailing_var_arg = true)]
 pub struct Args {
     /// Package to install
     package: String,
@@ -41,7 +50,7 @@ impl BinDir {
 fn bin_dir() -> anyhow::Result<PathBuf> {
     Ok(home_dir()
         .ok_or_else(|| anyhow::anyhow!("could not find home directory"))?
-        .join(".pax/bin"))
+        .join(BIN_DIR))
 }
 
 struct BinEnvDir(pub PathBuf);
@@ -59,7 +68,7 @@ impl BinEnvDir {
 fn bin_env_dir() -> anyhow::Result<PathBuf> {
     Ok(home_dir()
         .ok_or_else(|| anyhow::anyhow!("could not find home directory"))?
-        .join(".pax/envs"))
+        .join(BIN_ENVS_DIR))
 }
 
 /// Find the designated package in the prefix
@@ -75,21 +84,45 @@ async fn find_designated_package(
 }
 
 /// Create the environment activation script
-fn create_activation_script(prefix: &Prefix) -> anyhow::Result<String> {
-    let shell_type = rattler_shell::shell::ShellEnum::detect_from_environment()
-        .ok_or_else(|| anyhow::anyhow!("Could not detect shell type"))?;
-    let activator = Activator::from_path(prefix.root(), shell_type, Platform::Osx64)?;
+fn create_activation_script(prefix: &Prefix, shell: ShellEnum) -> anyhow::Result<String> {
+    let activator = Activator::from_path(prefix.root(), shell, Platform::Osx64)?;
     let result = activator.activation(ActivationVariables {
         conda_prefix: None,
-        path: Some(vec![
-            PathBuf::from("/usr/bin"),
-            PathBuf::from("/bin"),
-            PathBuf::from("/usr/sbin"),
-            PathBuf::from("/sbin"),
-            PathBuf::from("/usr/local/bin"),
-        ]),
+        path: None,
     })?;
     Ok(result.script)
+}
+
+fn is_executable(prefix: &Prefix, relative_path: &Path) -> bool {
+    // Check if the file is in a known executable directory.
+    let binary_folders = if cfg!(windows) {
+        &([
+            "",
+            "Library/mingw-w64/bin/",
+            "Library/usr/bin/",
+            "Library/bin/",
+            "Scripts/",
+            "bin/",
+        ][..])
+    } else {
+        &(["bin"][..])
+    };
+
+    let parent_folder = match relative_path.parent() {
+        Some(dir) => dir,
+        None => return false,
+    };
+
+    if !binary_folders
+        .iter()
+        .any(|bin_path| Path::new(bin_path) == parent_folder)
+    {
+        return false;
+    }
+
+    // Check if the file is executable
+    let absolute_path = prefix.root().join(relative_path);
+    is_executable::is_executable(absolute_path)
 }
 
 /// Create the executable scripts by modifying the activation script
@@ -97,40 +130,61 @@ fn create_activation_script(prefix: &Prefix) -> anyhow::Result<String> {
 async fn create_executable_scripts(
     prefix: &Prefix,
     prefix_package: &PrefixRecord,
+    shell: &ShellEnum,
     activation_script: String,
-) -> anyhow::Result<()> {
+) -> anyhow::Result<Vec<String>> {
     let executables = prefix_package
         .files
         .iter()
-        .filter(|f| f.starts_with("bin/") && is_executable::is_executable(prefix.root().join(f)));
+        .filter(|relative_path| is_executable(prefix, relative_path));
 
+    let mut scripts = Vec::new();
     let bin_dir = BinDir::create().await?;
     for exec in executables {
-        let script = activation_script.clone().add(&format!(
-            "\n ${{CONDA_PREFIX}}/{}",
-            exec.to_str().expect("could not convert path to string")
-        ));
-        let filename =
-            bin_dir.0.join(exec.file_name().ok_or_else(|| {
-                anyhow::anyhow!("could not get filename from {}", exec.display())
-            })?);
-        tokio::fs::write(&filename, script).await?;
+        let mut script = activation_script.clone();
+        shell
+            .run_command(
+                &mut script,
+                [
+                    prefix.root().join(exec).to_string_lossy().as_ref(),
+                    get_catch_all_arg(shell),
+                ],
+            )
+            .expect("should never fail");
+
+        let file_name = exec
+            .file_stem()
+            .ok_or_else(|| anyhow::anyhow!("could not get filename from {}", exec.display()))?;
+        let mut executable_script_path = bin_dir.0.join(file_name);
+
+        if cfg!(windows) {
+            executable_script_path.set_extension("bat");
+        };
+
+        tokio::fs::write(&executable_script_path, script).await?;
+
         #[cfg(unix)]
         {
             use std::os::unix::fs::PermissionsExt;
-            std::fs::set_permissions(filename, std::fs::Permissions::from_mode(0o744))?;
+            std::fs::set_permissions(
+                executable_script_path,
+                std::fs::Permissions::from_mode(0o744),
+            )?;
         }
+
+        scripts.push(file_name.to_string_lossy().into_owned());
     }
-    Ok(())
+    Ok(scripts)
 }
 
 /// Install a global command
 pub async fn execute(args: Args) -> anyhow::Result<()> {
     // Figure out what channels we are using
+    let channel_config = ChannelConfig::default();
     let channels = args
         .channels
         .iter()
-        .map(|c| Channel::from_str(c, &ChannelConfig::default()))
+        .map(|c| Channel::from_str(c, &channel_config))
         .collect::<Result<Vec<Channel>, _>>()?;
 
     // Find the matchspec we want to install
@@ -142,16 +196,6 @@ pub async fn execute(args: Args) -> anyhow::Result<()> {
         )
     })?;
     let platform = Platform::current();
-
-    println!(
-        "Installing: {}, from {}",
-        package_matchspec,
-        channels
-            .iter()
-            .map(friendly_channel_name)
-            .collect::<Vec<_>>()
-            .join(", ")
-    );
 
     // Fetch sparse repodata
     let platform_sparse_repodata = fetch_sparse_repodata(&channels, &vec![platform]).await?;
@@ -169,9 +213,14 @@ pub async fn execute(args: Args) -> anyhow::Result<()> {
             .iter()
             .map(|records| LibsolvRepoData::from_records(records)),
 
+        virtual_packages: rattler_virtual_packages::VirtualPackage::current()?
+            .iter()
+            .cloned()
+            .map(Into::into)
+            .collect(),
+
         locked_packages: vec![],
         pinned_packages: vec![],
-        virtual_packages: vec![],
     };
 
     // Solve it
@@ -190,7 +239,7 @@ pub async fn execute(args: Args) -> anyhow::Result<()> {
     if !transaction.operations.is_empty() {
         // Execute the operations that are returned by the solver.
         await_in_progress(
-            "installing environment",
+            "creating virtual environment",
             execute_transaction(
                 transaction,
                 prefix.root().to_path_buf(),
@@ -201,9 +250,85 @@ pub async fn execute(args: Args) -> anyhow::Result<()> {
         .await?;
     }
 
+    // Find the installed package in the environment
     let prefix_package = find_designated_package(&prefix, &package_name).await?;
-    let activation_script = create_activation_script(&prefix)?;
-    create_executable_scripts(&prefix, &prefix_package, activation_script).await?;
+    let channel = Channel::from_str(&prefix_package.repodata_record.channel, &channel_config)
+        .map(|ch| friendly_channel_name(&ch))
+        .unwrap_or_else(|_| prefix_package.repodata_record.channel.clone());
+
+    // Determine the shell to use for the invocation script
+    let shell: ShellEnum = if cfg!(windows) {
+        rattler_shell::shell::CmdExe.into()
+    } else {
+        rattler_shell::shell::Bash.into()
+    };
+
+    // Construct the reusable activation script for the shell and generate an invocation script
+    // for each executable added by the package to the environment.
+    let activation_script = create_activation_script(&prefix, shell.clone())?;
+    let script_names =
+        create_executable_scripts(&prefix, &prefix_package, &shell, activation_script).await?;
+
+    // Check if the bin path is on the path
+    if script_names.is_empty() {
+        anyhow::bail!(
+            "could not find an executable entrypoint in package {} {} {} from {}, are you sure it exists?",
+            console::style(prefix_package.repodata_record.package_record.name).bold(),
+            console::style(prefix_package.repodata_record.package_record.version).bold(),
+            console::style(prefix_package.repodata_record.package_record.build).bold(),
+            channel,
+        );
+    } else {
+        let whitespace = console::Emoji("  ", "").to_string();
+        eprintln!(
+            "{}Installed package {} {} {} from {}",
+            console::style(console::Emoji("✔ ", "")).green(),
+            console::style(prefix_package.repodata_record.package_record.name).bold(),
+            console::style(prefix_package.repodata_record.package_record.version).bold(),
+            console::style(prefix_package.repodata_record.package_record.build).bold(),
+            channel,
+        );
+
+        let script_names = script_names
+            .into_iter()
+            .join(&format!("\n{whitespace} -  "));
+
+        if is_bin_folder_on_path() {
+            eprintln!(
+                "{whitespace}These apps are now globally available:\n{whitespace} -  {script_names}",
+            )
+        } else {
+            let bin_dir = format!("~/{BIN_DIR}");
+            eprintln!("{whitespace}These apps have been added to {}\n{whitespace} -  {script_names}\n\n{} To use them, make sure to add {} to your PATH",
+                      console::style(&bin_dir).bold(),
+                      console::style("!").yellow().bold(),
+                      console::style(&bin_dir).bold()
+            )
+        }
+    }
 
     Ok(())
+}
+
+/// Returns the string to add for all arguments passed to the script
+fn get_catch_all_arg(shell: &ShellEnum) -> &str {
+    match shell {
+        ShellEnum::CmdExe(_) => "\"%*\"",
+        ShellEnum::PowerShell(_) => "@args",
+        _ => "\"$@\"",
+    }
+}
+
+/// Returns true if the bin folder is available on the PATH.
+fn is_bin_folder_on_path() -> bool {
+    let bin_path = match bin_dir() {
+        Ok(path) => path,
+        Err(_) => return false,
+    };
+
+    std::env::var_os("PATH")
+        .map(|path| std::env::split_paths(&path).collect_vec())
+        .unwrap_or_default()
+        .into_iter()
+        .contains(&bin_path)
 }
