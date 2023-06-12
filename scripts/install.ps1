1abc83c0fe93bd2f09e2186ac87547117a0462f4
Ruben Arts <ruben.arts@hotmail.com>
2023-06-12 14:19:45 +0200

    feat: add install scripts

---
+++ scripts/install.ps1
@@ -0,0 +1,49 @@
+param (
+    [string]$PIXI_VERSION = "latest",
+    [string]$PIXI_DIR = "$HOME/.pixi/bin"
+)
+
+# Repository name
+$REPO = "prefix-dev/pixi"
+$ARCH = "x86_64"
+$PLATFORM = "pc-windows-msvc"
+
+$BINARY = "pixi-$ARCH-$PLATFORM"
+
+if ($PIXI_VERSION -eq "latest") {
+    $DOWNLOAD_URL = "https://github.com/$REPO/releases/latest/download/$BINARY.zip"
+} else {
+    $DOWNLOAD_URL = "https://github.com/$REPO/releases/download/$PIXI_VERSION/$BINARY.zip"
+}
+
+Write-Host "This script will automatically download and install Pixi ($PIXI_VERSION) for you."
+Write-Host "Getting it from this url: $DOWNLOAD_URL"
+Write-Host "The binary will be installed into '$PIXI_DIR'"
+
+$TEMP_FILE = [System.IO.Path]::GetTempFileName()
+
+try {
+    Invoke-WebRequest -Uri $DOWNLOAD_URL -OutFile $TEMP_FILE
+
+    # Create the install dir if it doesn't exist
+    if (!(Test-Path -Path $PIXI_DIR )) {
+        New-Item -ItemType directory -Path $PIXI_DIR
+    }
+
+    # Extract pixi from the downloaded zip file
+    Expand-Archive -Path $TEMP_FILE -DestinationPath $PIXI_DIR -Force
+
+    # Get the name of the extracted folder
+    $EXTRACTED_FOLDER = Get-ChildItem -Path $PIXI_DIR -Directory | Sort-Object LastAccessTime -Descending | Select-Object -First 1
+
+    # Move the pixi bin to the install dir
+    Move-Item -Path "$PIXI_DIR/$EXTRACTED_FOLDER/pixi" -Destination "$PIXI_DIR/pixi"
+
+    Write-Host "Installation complete. Please add '$PIXI_DIR' to your PATH."
+
+} catch {
+    Write-Host "Error: '$DOWNLOAD_URL' is not available or failed to download"
+    exit 1
+} finally {
+    Remove-Item -Path $TEMP_FILE
+}
---
+++ scripts/install.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+__wrap__() {
+
+VERSION=${PIXI_VERSION:-latest}
+INSTALL_DIR=${PIXI_DIR:-"$HOME/.pixi/bin"}
+
+REPO=prefix-dev/pixi
+PLATFORM=$(uname -s)
+ARCH=$(uname -m)
+
+if [[ $PLATFORM == "Darwin" ]]; then
+  PLATFORM="apple-darwin"
+elif [[ $PLATFORM == "Linux" ]]; then
+  PLATFORM="unknown-linux-musl"
+fi
+
+if [[ $ARCH == "arm64" ]] || [[ $ARCH == "aarch64" ]]; then
+  ARCH="aarch64"
+fi
+
+
+
+BINARY="pixi-${ARCH}-${PLATFORM}"
+
+if [[ $VERSION == "latest" ]]; then
+  DOWNLOAD_URL=https://github.com/${REPO}/releases/latest/download/${BINARY}.tar.gz
+else
+  DOWNLOAD_URL=https://github.com/${REPO}/releases/download/${VERSION}/${BINARY}.tar.gz
+fi
+
+printf "This script will automatically download and install Pixi (${VERSION}) for you.\nGetting it from this url: $DOWNLOAD_URL\nThe binary will be installed into '$INSTALL_DIR'\n"
+
+if ! hash curl 2> /dev/null; then
+  echo "error: you do not have 'curl' installed which is required for this script."
+  exit 1
+fi
+
+if ! hash tar 2> /dev/null; then
+  echo "error: you do not have 'tar' installed which is required for this script."
+  exit 1
+fi
+
+TEMP_FILE=$(mktemp "${TMPDIR:-/tmp}/.pixi_install.XXXXXXXX")
+
+cleanup() {
+  rm -f "$TEMP_FILE"
+}
+
+trap cleanup EXIT
+
+HTTP_CODE=$(curl -SL --progress-bar "$DOWNLOAD_URL" --output "$TEMP_FILE" --write-out "%{http_code}")
+if [[ ${HTTP_CODE} -lt 200 || ${HTTP_CODE} -gt 299 ]]; then
+  echo "error: '${DOWNLOAD_URL}' is not available"
+  exit 1
+fi
+
+# Extract pixi from the downloaded tar file
+mkdir -p "$INSTALL_DIR"
+tar -xzf "$TEMP_FILE" -C "$INSTALL_DIR"
+
+# Get the name of the extracted folder
+EXTRACTED_FOLDER=$(ls -lt "$INSTALL_DIR" | grep '^d' | head -n1 | awk '{print $NF}')
+
+# Move the pixi bin to the install dir
+mv "$INSTALL_DIR/$EXTRACTED_FOLDER/pixi" "$INSTALL_DIR/pixi"
+
+# Make it executable and add it to the path.
+echo "export PATH=$PATH:${INSTALL_DIR}" >> ~/.bash_profile
+chmod +x "$INSTALL_DIR/pixi"
+
+echo "Please restart or source your shell."
+
+}; __wrap__
