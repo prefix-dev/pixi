96f3361425062255b39c7264c0cefaee1e74c2e1
Wolf Vollprecht <w.vollprecht@gmail.com>
2023-06-22 15:29:48 +0200

    add draft for uploading packages (to prefix)

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -13,6 +13,7 @@ mod auth;
 mod global;
 mod init;
 mod run;
+mod upload;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -45,6 +46,7 @@ enum Command {
     #[clap(alias = "g")]
     Global(global::Args),
     Auth(auth::Args),
+    Upload(upload::Args)
 }
 
 fn completion(args: CompletionCommand) -> Result<(), Error> {
@@ -105,6 +107,7 @@ pub async fn execute() -> anyhow::Result<()> {
         Some(Command::Run(cmd)) => run::execute(cmd).await,
         Some(Command::Global(cmd)) => global::execute(cmd).await,
         Some(Command::Auth(cmd)) => auth::execute(cmd).await,
+        Some(Command::Upload(cmd)) => upload::execute(cmd).await,
         None => default().await,
     }
 }
---
+++ src/cli/upload.rs
@@ -0,0 +1,48 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+use rattler_networking::AuthenticatedClient;
+
+use rattler_digest::{compute_file_digest, Sha256};
+
+/// Upload a package to a prefix.dev channel
+#[derive(Parser, Debug)]
+pub struct Args {
+    /// The host + channel to upload to
+    host: String,
+
+    /// The file to upload
+    package_file: PathBuf,
+}
+
+
+/// Upload a package to a prefix.dev channel
+pub async fn execute(args: Args) -> anyhow::Result<()> {
+    println!("Uploading package to {}...", args.host);
+    println!("Package file: {}", args.package_file.display());
+
+    let client = AuthenticatedClient::default();
+
+    let sha256sum = format!("{:x}", compute_file_digest::<Sha256>(&args.package_file)?);
+    let filename = args.package_file.file_name().unwrap().to_string_lossy().to_string();
+    let filesize = args.package_file.metadata()?.len();
+
+    let response = client
+        .post(args.host.clone())
+        .header("X-File-Sha256", sha256sum)
+        .header("X-File-Name", filename)
+        .header("Content-Length", filesize)
+        .header("Content-Type", "application/octet-stream")
+        .body(std::fs::read(&args.package_file)?)
+        .send()
+        .await?
+    ;
+
+    println!("Response: {:?}", response);
+
+
+
+    println!("Done!");
+
+    Ok(())
+}
