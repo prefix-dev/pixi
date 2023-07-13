d8c2970057121daac24fd92a9fd8f9ecb75a33df
Wolf Vollprecht <w.vollprecht@gmail.com>
2023-07-13 15:03:56 +0200

    add build command

---
+++ src/cli/build.rs
@@ -0,0 +1,166 @@
+use std::{path::PathBuf, str::FromStr};
+
+use clap::Parser;
+use rattler_build::{
+    build::run_build,
+    metadata::{About, BuildOptions, Output, Package, PathSrc, RenderedRecipe, Requirements},
+    render::dependency_list::Dependency,
+    tool_configuration::Configuration,
+};
+use rattler_conda_types::{MatchSpec, NoArchType, Platform};
+
+use crate::{
+    task::{CmdArgs, Task},
+    Project,
+};
+
+/// Build the project into a conda package.
+#[derive(Parser, Debug)]
+pub struct Args {}
+
+fn get_script(project: &Project) -> String {
+    // find the "install" script and all the dependencies
+
+    let mut script: Vec<String> = Vec::new();
+
+    let get_script = |x: &Task| match x {
+        Task::Plain(cmd) => cmd.clone(),
+        Task::Execute(cmd) => match &cmd.cmd {
+            CmdArgs::Single(cmd) => cmd.to_string(),
+            CmdArgs::Multiple(cmd_vec) => cmd_vec.join(" "),
+        },
+        Task::Alias(cmd) => cmd.depends_on.join(" && "),
+    };
+
+    let mut dependencies: Vec<String> = Vec::new();
+    let task = project
+        .task_opt("install")
+        .expect("Could not find an install task");
+
+    script.push(get_script(task));
+
+    dependencies.extend(task.depends_on().iter().cloned());
+
+    while !dependencies.is_empty() {
+        let task = project
+            .task_opt(&dependencies[0])
+            .expect("Could not find a task");
+        script.push(get_script(task));
+
+        dependencies.extend(task.depends_on().iter().cloned());
+        dependencies.remove(0);
+    }
+
+    script.reverse();
+    script.join("\n")
+}
+
+pub async fn execute(_args: Args) -> anyhow::Result<()> {
+    let project = Project::discover()?;
+
+    let directories = rattler_build::metadata::Directories::create(
+        project.name(),
+        project.root(),
+        &PathBuf::from("target"),
+    )?;
+
+    let channels = project.manifest.project.channels.clone();
+
+    let target_platform = Platform::current();
+
+    let build_configuration = rattler_build::metadata::BuildConfiguration {
+        target_platform,
+        host_platform: Platform::current(),
+        build_platform: Platform::current(),
+        variant: Default::default(),
+        hash: "123123".to_string(),
+        no_clean: false,
+        directories,
+        channels: channels.iter().map(|c| c.canonical_name()).collect(),
+        timestamp: chrono::Utc::now(),
+        subpackages: Default::default(),
+    };
+
+    let script = get_script(&project);
+
+    println!("Script: {}", script);
+
+    let host_dependencies: Vec<Dependency> = project
+        .host_dependencies(target_platform)?
+        .into_iter()
+        .map(|dep| {
+            let ms = MatchSpec::from_str(&format!("{} {}", dep.0, dep.1)).unwrap();
+            Dependency::Spec(ms)
+        })
+        .collect();
+
+    let _build_dependencies: Vec<Dependency> = project
+        .build_dependencies(target_platform)?
+        .into_iter()
+        .map(|dep| {
+            let ms = MatchSpec::from_str(&format!("{} {}", dep.0, dep.1)).unwrap();
+            Dependency::Spec(ms)
+        })
+        .collect();
+
+    let _dependencies: Vec<Dependency> = project
+        .dependencies(target_platform)?
+        .into_iter()
+        .map(|dep| {
+            let ms = MatchSpec::from_str(&format!("{} {}", dep.0, dep.1)).unwrap();
+            Dependency::Spec(ms)
+        })
+        .collect();
+
+    let recipe = RenderedRecipe {
+        source: Some(vec![rattler_build::metadata::Source::Path(PathSrc {
+            path: project.root().to_path_buf(),
+            patches: Default::default(),
+            folder: None,
+        })]),
+        build: BuildOptions {
+            number: 0,
+            // todo - how do we compute the build hash?
+            string: Some("0".to_string()),
+            script: Some(vec![script]),
+            ignore_run_exports: None,
+            ignore_run_exports_from: None,
+            run_exports: None,
+            noarch: NoArchType::none(),
+            entry_points: Vec::default(),
+        },
+        requirements: Requirements {
+            build: Vec::default(),
+            host: host_dependencies,
+            run: Vec::default(),
+            run_constrained: Vec::default(),
+        },
+        about: About {
+            home: None,
+            license: None,
+            license_file: None,
+            license_family: None,
+            summary: None,
+            description: project.manifest.project.description.clone(),
+            doc_url: None,
+            dev_url: None,
+        },
+        package: Package {
+            name: project.name().to_string(),
+            version: project.version().to_string(),
+        },
+        test: None,
+    };
+
+    let output = Output {
+        recipe,
+        build_configuration,
+        finalized_dependencies: None,
+    };
+
+    let configuration = Configuration::default();
+
+    run_build(&output, configuration).await?;
+
+    Ok(())
+}
