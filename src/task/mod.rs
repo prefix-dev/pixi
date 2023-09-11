26c2af14b28de8f4dec89e37c3b58ff072445e1d
Bas Zalmstra <bas@prefix.dev>
2023-09-11 13:58:45 +0200

    fix: simplify quoting logic (#313)

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -1,6 +1,7 @@
 use itertools::Itertools;
 use serde::Deserialize;
 use serde_with::{formats::PreferMany, serde_as, OneOrMany};
+use std::borrow::Cow;
 use std::fmt::{Display, Formatter};
 
 /// Represents different types of scripts
@@ -13,6 +14,7 @@ pub enum Task {
 }
 
 impl Task {
+    /// Returns the names of the task that this task depends on
     pub fn depends_on(&self) -> &[String] {
         match self {
             Task::Plain(_) => &[],
@@ -52,6 +54,24 @@ impl Task {
             Task::Alias(_) => false,
         }
     }
+
+    /// Returns the command to execute.
+    pub fn as_command(&self) -> Option<CmdArgs> {
+        match self {
+            Task::Plain(cmd) => Some(CmdArgs::Single(cmd.clone())),
+            Task::Execute(exe) => Some(exe.cmd.clone()),
+            Task::Alias(_) => None,
+        }
+    }
+
+    /// Returns the command to execute as a single string.
+    pub fn as_single_command(&self) -> Option<Cow<str>> {
+        match self {
+            Task::Plain(cmd) => Some(Cow::Borrowed(cmd)),
+            Task::Execute(exe) => Some(exe.cmd.as_single()),
+            Task::Alias(_) => None,
+        }
+    }
 }
 
 /// A command script executes a single command from the environment
@@ -68,11 +88,47 @@ pub struct Execute {
     pub depends_on: Vec<String>,
 }
 
+impl From<Execute> for Task {
+    fn from(value: Execute) -> Self {
+        Task::Execute(value)
+    }
+}
+
 #[derive(Debug, Clone, Deserialize)]
 #[serde(untagged)]
 pub enum CmdArgs {
     Single(String),
     Multiple(Vec<String>),
+}
+
+impl From<Vec<String>> for CmdArgs {
+    fn from(value: Vec<String>) -> Self {
+        CmdArgs::Multiple(value)
+    }
+}
+
+impl From<String> for CmdArgs {
+    fn from(value: String) -> Self {
+        CmdArgs::Single(value)
+    }
+}
+
+impl CmdArgs {
+    /// Returns a single string representation of the command arguments.
+    pub fn as_single(&self) -> Cow<str> {
+        match self {
+            CmdArgs::Single(cmd) => Cow::Borrowed(cmd),
+            CmdArgs::Multiple(args) => Cow::Owned(args.iter().map(|arg| quote(arg)).join(" ")),
+        }
+    }
+
+    /// Returns a single string representation of the command arguments.
+    pub fn into_single(self) -> String {
+        match self {
+            CmdArgs::Single(cmd) => cmd,
+            CmdArgs::Multiple(args) => args.iter().map(|arg| quote(arg)).join(" "),
+        }
+    }
 }
 
 #[derive(Debug, Clone, Deserialize)]
@@ -112,5 +168,54 @@ impl Display for Task {
         } else {
             Ok(())
         }
+    }
+}
+
+/// Quotes a string argument if it requires quotes to be able to be properly represented in our
+/// shell implementation.
+pub fn quote(in_str: &str) -> Cow<str> {
+    if in_str.is_empty() {
+        "\"\"".into()
+    } else if in_str
+        .bytes()
+        .any(|c| matches!(c as char, '\t' | '\r' | '\n' | ' '))
+    {
+        let mut out: Vec<u8> = Vec::new();
+        out.push(b'"');
+        for c in in_str.bytes() {
+            match c as char {
+                '"' | '\\' => out.push(b'\\'),
+                _ => (),
+            }
+            out.push(c);
+        }
+        out.push(b'"');
+        unsafe { String::from_utf8_unchecked(out) }.into()
+    } else {
+        in_str.into()
+    }
+}
+
+/// Quotes multiple string arguments and joins them together to form a single string.
+pub fn quote_arguments<'a>(args: impl IntoIterator<Item = &'a str>) -> String {
+    args.into_iter().map(quote).join(" ")
+}
+
+#[cfg(test)]
+mod test {
+    use super::quote;
+
+    #[test]
+    fn test_quote() {
+        assert_eq!(quote("foobar"), "foobar");
+        assert_eq!(quote("foo bar"), "\"foo bar\"");
+        assert_eq!(quote("\""), "\"");
+        assert_eq!(quote("foo \" bar"), "\"foo \\\" bar\"");
+        assert_eq!(quote(""), "\"\"");
+        assert_eq!(quote("$PATH"), "$PATH");
+        assert_eq!(
+            quote("PATH=\"$PATH;build/Debug\""),
+            "PATH=\"$PATH;build/Debug\""
+        );
     }
 }
