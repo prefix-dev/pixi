2d77349167224a41eff03d8af13b6983b0e04514
Ruben Arts <ruben.arts@hotmail.com>
2023-10-17 13:08:50 +0200

    wip: start zsh support

--- src/cli/completion.rs
+++ src/cli/completion.rs
@@ -29,6 +29,12 @@ pub(crate) fn execute(args: CompletionCommand) -> miette::Result<()> {
                 .write_all(script.as_ref().as_ref())
                 .into_diagnostic()?;
         }
+        clap_complete::Shell::Zsh => {
+            let script = replace_zsh_completion(from_utf8_mut(&mut script).into_diagnostic()?);
+            std::io::stdout()
+                .write_all(script.as_ref().as_ref())
+                .into_diagnostic()?;
+        }
         _ => {
             // If no replacements needed write original script to stdout
             std::io::stdout().write_all(&script).into_diagnostic()?;
@@ -56,6 +62,27 @@ fn replace_bash_completion(script: &str) -> Cow<str> {
     let re = Regex::new(pattern).unwrap();
     re.replace(script, replacement)
 }
+
+fn replace_zsh_completion(script: &str) -> Cow<str> {
+    let pattern = r#"(?s)pixi__run\).*?opts="(.*?)".*?(if.*?fi)"#;
+
+    let zsh_replacement = r#"pixi__run)
+            opts="$1"
+            if [[ $${CURRENT} -eq 2 ]]; then
+                local tasks=$$(pixi task list --summary 2> /dev/null)
+                if [[ $$? -eq 0 ]]; then
+                    compadd "$${tasks}"
+                    return 1
+                fi
+            elif [[ $${cur} == -* ]]; then
+                compadd -- "$${opts}"
+                return 1
+            fi"#;
+
+    let re = Regex::new(pattern).unwrap();
+    re.replace(script, zsh_replacement)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
