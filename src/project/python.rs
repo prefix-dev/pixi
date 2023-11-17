3e6fbf09e80646e7f0e27f92e454afccc709d074
Ruben Arts <ruben.arts@hotmail.com>
2023-11-17 16:00:14 +0100

    wip: Use custom requirement type

--- src/project/python.rs
+++ src/project/python.rs
@@ -1,18 +1,52 @@
-use crate::utils::spanned::PixiSpanned;
 use indexmap::IndexMap;
-use serde::de::{DeserializeSeed, Error, MapAccess, Visitor};
-use serde::{Deserialize, Deserializer};
-use std::collections::HashMap;
+use pep440_rs::VersionSpecifiers;
+use pep508_rs::VersionOrUrl;
+use serde::de::{Error, MapAccess, Visitor};
+use serde::{de, Deserialize, Deserializer};
 use std::fmt::Formatter;
+use std::str::FromStr;
+use thiserror::Error;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct PyPiRequirement {
+    version: Option<VersionSpecifiers>,
+    extras: Option<Vec<String>>,
+}
+
+/// The type of parse error that occurred when parsing match spec.
+#[derive(Debug, Clone, Error)]
+pub enum ParsePyPiRequirementError {
+    #[error("invalid PEP440")]
+    Pep440Error(#[from] pep440_rs::Pep440Error),
+}
+
+impl FromStr for PyPiRequirement {
+    type Err = ParsePyPiRequirementError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.is_empty() || s == "*" {
+            Ok(Self {
+                version: None,
+                extras: None,
+            })
+        } else {
+            Ok(Self {
+                version: Some(
+                    VersionSpecifiers::from_str(s)
+                        .map_err(ParsePyPiRequirementError::Pep440Error)?,
+                ),
+                extras: None,
+            })
+        }
+    }
+}
 
 /// Represents a set of python dependencies on which a project can depend. The dependencies are
 /// formatted using a modern version specifier. See [`pep508_rs::modern::RequirementModern`].
-#[derive(Default, Debug, Clone)]
+#[derive(Default, Debug, Deserialize, Clone, Eq, PartialEq)]
 pub struct PypiDependencies {
-    requirements: IndexMap<
-        PixiSpanned<rip::PackageName>,
-        PixiSpanned<(pep508_rs::modern::RequirementModern, pep508_rs::Requirement)>,
-    >,
+    #[serde(flatten)]
+    requirements: IndexMap<rip::PackageName, PyPiRequirement>,
 }
 
 impl PypiDependencies {
@@ -24,106 +58,167 @@ impl PypiDependencies {
     /// Returns the requirements as [`pep508_rs::Requirement`]s.
     pub fn as_pep508(&self) -> Vec<pep508_rs::Requirement> {
         self.requirements
-            .values()
-            .map(|s| s.value.1.clone())
+            .clone()
+            .into_iter()
+            .map(|(name, req)| {
+                let version = req.version.map(VersionOrUrl::VersionSpecifier);
+
+                pep508_rs::Requirement {
+                    name: name.as_str().to_string(),
+                    extras: req.extras,
+                    version_or_url: version,
+                    marker: None,
+                }
+            })
             .collect()
     }
 }
-
-impl<'de> Deserialize<'de> for PypiDependencies {
+impl<'de> Deserialize<'de> for PyPiRequirement {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
     {
-        struct RequirementsVisitor;
-        struct RequirementVisitor<'i> {
-            name: &'i str,
-        }
-
-        impl<'de, 'i> DeserializeSeed<'de> for RequirementVisitor<'i> {
-            type Value =
-                PixiSpanned<(pep508_rs::modern::RequirementModern, pep508_rs::Requirement)>;
-
-            fn deserialize<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
-            where
-                D: Deserializer<'de>,
-            {
-                let PixiSpanned { value, span } =
-                    PixiSpanned::<pep508_rs::modern::RequirementModern>::deserialize(deserializer)?;
-                let req = value
-                    .to_pep508(self.name, &HashMap::default())
-                    .map_err(D::Error::custom)?;
-                Ok(PixiSpanned {
-                    value: (value, req),
-                    span,
-                })
-            }
-        }
-
-        impl<'de> Visitor<'de> for RequirementsVisitor {
-            type Value = IndexMap<
-                PixiSpanned<rip::PackageName>,
-                PixiSpanned<(pep508_rs::modern::RequirementModern, pep508_rs::Requirement)>,
-            >;
+        struct RequirementVisitor;
+        impl<'de> Visitor<'de> for RequirementVisitor {
+            type Value = PyPiRequirement;
 
             fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
-                formatter.write_str("a mapping from package names to requirements")
+                formatter.write_str("a mapping from package names to a pypi requirement")
             }
-
-            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: Error,
+            {
+                PyPiRequirement::from_str(v).map_err(Error::custom)
+            }
+            fn visit_map<A>(self, map: A) -> Result<Self::Value, A::Error>
             where
                 A: MapAccess<'de>,
             {
-                let mut result = IndexMap::new();
-                while let Some(key) = map.next_key::<PixiSpanned<rip::PackageName>>()? {
-                    let value = map.next_value_seed(RequirementVisitor {
-                        name: key.value.as_str(),
-                    })?;
-                    result.insert(key, value);
+                // Use a temp struct to deserialize into when it is a map.
+                #[derive(Deserialize)]
+                struct RawPyPiRequirement {
+                    version: Option<VersionSpecifiers>,
+                    extras: Option<Vec<String>>,
                 }
-                Ok(result)
+                let raw_requirement =
+                    RawPyPiRequirement::deserialize(de::value::MapAccessDeserializer::new(map))?;
+                Ok(PyPiRequirement {
+                    version: raw_requirement.version,
+                    extras: raw_requirement.extras,
+                })
             }
         }
-
-        deserializer
-            .deserialize_map(RequirementsVisitor {})
-            .map(|requirements| PypiDependencies { requirements })
+        deserializer.deserialize_any(RequirementVisitor {})
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use insta::assert_debug_snapshot;
 
     #[test]
     fn test_only_version() {
-        let requirement: PypiDependencies = toml_edit::de::from_str("foo = \">=3.12\"").unwrap();
-        assert_debug_snapshot!(requirement);
+        let requirement: PypiDependencies = toml_edit::de::from_str(r#"foo = ">=3.12""#).unwrap();
+        assert_eq!(
+            requirement,
+            PypiDependencies {
+                requirements: IndexMap::from([(
+                    rip::PackageName::from_str("foo").unwrap(),
+                    PyPiRequirement {
+                        version: Some(VersionSpecifiers::from_str(">=3.12").unwrap()),
+                        extras: None
+                    }
+                ),])
+            }
+        );
+        let requirement: PypiDependencies = toml_edit::de::from_str(r#"foo = "==3.12.0""#).unwrap();
+        assert_eq!(
+            requirement,
+            PypiDependencies {
+                requirements: IndexMap::from([(
+                    rip::PackageName::from_str("foo").unwrap(),
+                    PyPiRequirement {
+                        version: Some(VersionSpecifiers::from_str("==3.12.0").unwrap()),
+                        extras: None
+                    }
+                ),])
+            }
+        );
+        let requirement: PypiDependencies = toml_edit::de::from_str(r#"foo = "~=2.1.3""#).unwrap();
+        assert_eq!(
+            requirement,
+            PypiDependencies {
+                requirements: IndexMap::from([(
+                    rip::PackageName::from_str("foo").unwrap(),
+                    PyPiRequirement {
+                        version: Some(VersionSpecifiers::from_str("~=2.1.3").unwrap()),
+                        extras: None
+                    }
+                ),])
+            }
+        );
+        let requirement: PypiDependencies = toml_edit::de::from_str(r#"foo = """#).unwrap();
+        assert_eq!(
+            requirement,
+            PypiDependencies {
+                requirements: IndexMap::from([(
+                    rip::PackageName::from_str("foo").unwrap(),
+                    PyPiRequirement {
+                        version: None,
+                        extras: None
+                    }
+                ),])
+            }
+        );
+    }
+    #[test]
+    fn test_specifiers() {
+        // let test = Version::from_str("").unwrap();
+        // let test = VersionSpecifiers::from_str("").unwrap();
     }
 
     #[test]
     fn test_extended() {
         let requirement: PypiDependencies =
-            toml::de::from_str("foo = { version=\">=3.12\", extras = [\"bar\"] }").unwrap();
-        assert_debug_snapshot!(requirement);
-    }
-
-    #[test]
-    fn test_invalid_git_dependency_error() {
-        let requirement = toml::de::from_str::<PypiDependencies>(
-            "foo = { git=\"https://github.com/foo/bar\", branch = \"main\", rev=\"deadbeef\" }",
+            toml::de::from_str(r#"foo = { version=">=3.12", extras = ["bar"] }"#).unwrap();
+        assert_eq!(
+            requirement,
+            PypiDependencies {
+                requirements: IndexMap::from([(
+                    rip::PackageName::from_str("foo").unwrap(),
+                    PyPiRequirement {
+                        version: Some(VersionSpecifiers::from_str(">=3.12").unwrap()),
+                        extras: Some(vec!("bar".to_string()))
+                    }
+                ),])
+            }
         );
-        assert_debug_snapshot!(requirement.unwrap_err());
+
+        let requirement: PypiDependencies =
+            toml::de::from_str(r#"bar = { version=">=3.12,<3.13.0", extras = ["bar", "foo"] }"#)
+                .unwrap();
+        assert_eq!(
+            requirement,
+            PypiDependencies {
+                requirements: IndexMap::from([(
+                    rip::PackageName::from_str("bar").unwrap(),
+                    PyPiRequirement {
+                        version: Some(VersionSpecifiers::from_str(">=3.12,<3.13.0").unwrap()),
+                        extras: Some(vec!("bar".to_string(), "foo".to_string()))
+                    }
+                ),])
+            }
+        );
     }
 
     // TODO: This crashes currently but should instead result in an error. This should be fixed in pep508_rs.
-    //#[test]
+    // #[test]
     // fn test_invalid_extra() {
-    //     let requirement = toml::de::from_str::<PythonDependencies>(
-    //         r#"foo = { version="3.2.1", extras=["b$r"] }"#,
-    //     );
-    //     assert_debug_snapshot!(requirement.unwrap_err());
+    //     let requirement: PypiDependencies =
+    //         toml::de::from_str(r#"bar = { version="3.12", extras = ["b$r"] }"#)
+    //             .unwrap();
+    //
     // }
 
     // TODO: pep508_rs does not handle ^ operator yet.
