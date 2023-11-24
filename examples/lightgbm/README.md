01758b61eafb6e1daadf1e03c578df51e5455f5f
fkt <deepthought@postech.ac.kr>
2023-11-24 09:50:56 +0900

    add lightgbm demo

---
+++ examples/lightgbm/README.md
@@ -0,0 +1,25 @@
+# Applying LightGBM Classification Model to Wisconsin Breast Cancer Dataset
+
+This simple code to build and evaluate a LightGBM classification model using the Wisconsin Breast Cancer dataset.
+
+
+## Usage
+
+1. Check dataset: `breast_cancer_data.csv`
+2. Run the code:
+
+```bash
+pixi run python main.py
+```
+
+## Results
+
+After running the code, the model's training and testing accuracy will be displayed.
+
+```bash
+Model accuracy: 0.930
+True Postives(TP) = 55
+True Negatice(TN) = 104
+False Positive(FP) = 8
+False Negative(FN) = 4
+```
---
+++ examples/lightgbm/main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import lightgbm as lgb
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# load data
+df = pd.read_csv("Breast_cancer_data.csv")
+
+# Declare feature vector and target variable
+X = df[['mean_radius','mean_texture','mean_perimeter','mean_area','mean_smoothness']]
+y = df['diagnosis']
+
+# split the dataset into the training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# build the lightgbm model
+clf = lgb.LGBMClassifier(verbose=-1)
+clf.fit(X_train, y_train)
+
+# predict the results
+y_pred = clf.predict(X_test)
+
+# view accuracy
+accuracy = accuracy_score(y_pred, y_test)
+print(f"Model accuracy: {accuracy:0.3f}")
+
+# view confusion-matrix
+cm = confusion_matrix(y_test, y_pred)
+print("True Positives(TP) = ", cm[0,0])
+print("True Negatives(TN) = ", cm[1,1])
+print("False Positives(FP) = ", cm[0,1])
+print("False Negatives(FN) = ", cm[1,0])
