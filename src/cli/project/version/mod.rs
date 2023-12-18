5b41533707b8b58415d91fd9d67d2bc96956385a
Hadrien Mary <hadrien.mary@gmail.com>
2023-12-18 16:15:24 -0500

    draft of version bump cli

--- src/cli/project/version/mod.rs
+++ src/cli/project/version/mod.rs
@@ -1,3 +1,4 @@
+pub mod bump;
 pub mod get;
 pub mod set;
 
@@ -21,6 +22,9 @@ pub struct Args {
 pub enum Command {
     Get(get::Args),
     Set(set::Args),
+    Major(bump::Args),
+    Minor(bump::Args),
+    Patch(bump::Args),
 }
 
 pub async fn execute(args: Args) -> miette::Result<()> {
@@ -29,6 +33,9 @@ pub async fn execute(args: Args) -> miette::Result<()> {
     match args.command {
         Command::Get(args) => get::execute(project, args).await?,
         Command::Set(args) => set::execute(project, args).await?,
+        Command::Major(args) => bump::execute(bump::BumpType::Major, project, args).await?,
+        Command::Minor(args) => bump::execute(bump::BumpType::Minor, project, args).await?,
+        Command::Patch(args) => bump::execute(bump::BumpType::Patch, project, args).await?,
     }
 
     Ok(())
