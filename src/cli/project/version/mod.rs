d1ea1d45e21f7d02e31293c36bce24360791ec11
Hadrien Mary <hadrien.mary@gmail.com>
2023-12-19 08:56:10 -0500

    Remove bump command for now

--- src/cli/project/version/mod.rs
+++ src/cli/project/version/mod.rs
@@ -1,4 +1,3 @@
-pub mod bump;
 pub mod get;
 pub mod set;
 
@@ -22,9 +21,6 @@ pub struct Args {
 pub enum Command {
     Get(get::Args),
     Set(set::Args),
-    Major(bump::Args),
-    Minor(bump::Args),
-    Patch(bump::Args),
 }
 
 pub async fn execute(args: Args) -> miette::Result<()> {
@@ -33,9 +29,6 @@ pub async fn execute(args: Args) -> miette::Result<()> {
     match args.command {
         Command::Get(args) => get::execute(project, args).await?,
         Command::Set(args) => set::execute(project, args).await?,
-        Command::Major(args) => bump::execute(bump::BumpType::Major, project, args).await?,
-        Command::Minor(args) => bump::execute(bump::BumpType::Minor, project, args).await?,
-        Command::Patch(args) => bump::execute(bump::BumpType::Patch, project, args).await?,
     }
 
     Ok(())
