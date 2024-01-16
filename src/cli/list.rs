b8500739cb3b22d464595f0e0bbcc23586953399
Hadrien Mary <hadrien.mary@gmail.com>
2024-01-16 08:12:22 -0500

    use split_once

--- src/cli/list.rs
+++ src/cli/list.rs
@@ -242,8 +242,8 @@ fn create_package_to_output(
             // with a dedicated upstream function in rattler maybe.
             let name = match dirty_name {
                 Some(dirty_name) => dirty_name
-                    .split('/')
-                    .next()
+                    .split_once('/')
+                    .map(|(name, _)| name)
                     .unwrap_or(&dirty_name)
                     .to_string(),
                 None => "".to_string(),
