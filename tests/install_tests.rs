b8291704281d184a3297311741ca29aa387b74da
Tim de Jager <tim@prefix.dev>
2024-01-22 15:11:42 +0100

    fix: failing test on apple silicon, has been there for a bit

--- tests/install_tests.rs
+++ tests/install_tests.rs
@@ -115,7 +115,7 @@ async fn install_locked() {
     let pixi = PixiControl::new().unwrap();
     pixi.init().await.unwrap();
     // Add and update lockfile with this version of python
-    pixi.add("python==3.8.0").await.unwrap();
+    pixi.add("python==3.10.0").await.unwrap();
 
     // Add new version of python only to the manifest
     pixi.add("python==3.9.0")
@@ -127,7 +127,7 @@ async fn install_locked() {
 
     // Check if it didn't accidentally update the lockfile
     let lock = pixi.lock_file().await.unwrap();
-    assert!(lock.contains_matchspec("python==3.8.0"));
+    assert!(lock.contains_matchspec("python==3.10.0"));
 
     // After an install with lockfile update the locked install should succeed.
     pixi.install().await.unwrap();
