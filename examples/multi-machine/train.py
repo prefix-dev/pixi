f7c31400ae09ebdaff866ff3e2e20b21f06aa094
Ruben Arts <ruben.arts@hotmail.com>
2024-02-01 15:34:47 +0100

    misc: add another example

---
+++ examples/multi-machine/train.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+print("Hello from train.py!")
+print("Environment you are running on:")
+print(os.environ["PIXI_ENVIRONMENT_NAME"])
+print("Arguments given to the script:")
+print(sys.argv[1:])
