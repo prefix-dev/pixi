0ef2215bd574101fd6d7ec535d6b001e89f66e20
Ruben Arts <ruben.arts@hotmail.com>
2024-02-02 09:54:07 +0100

    test: HWA module in train.py

--- examples/multi-machine/train.py
+++ examples/multi-machine/train.py
@@ -1,7 +1,22 @@
 import os
 import sys
 
-print("Hello from train.py!")
+if os.environ["PIXI_ENVIRONMENT_NAME"] == "mlx":
+    import mlx
+    mlx.init()
+    print("MLX is available, in mlx environment as expected")
+
+if os.environ["PIXI_ENVIRONMENT_NAME"] == "cuda":
+    import torch
+    assert torch.cuda.is_available(), "CUDA is not available"
+    print("CUDA is available, in cuda environment as expected")
+
+if os.environ["PIXI_ENVIRONMENT_NAME"] == "default":
+    import torch
+    assert not torch.cuda.is_available(), "CUDA is available, in default environment"
+    print("CUDA is not available, in default environment as expected")
+
+print("\nHello from train.py!")
 print("Environment you are running on:")
 print(os.environ["PIXI_ENVIRONMENT_NAME"])
 print("Arguments given to the script:")
