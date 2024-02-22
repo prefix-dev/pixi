c30640eef5988f504247c900fd1cec6400445413
Tarun Pratap Singh <101409098+Wackyator@users.noreply.github.com>
2024-02-22 13:56:44 +0530

    feat: add import yml flag (#792)
    Co-authored-by: Bas Zalmstra <zalmstra.bas@gmail.com>
    Co-authored-by: Ruben Arts <ruben.arts@hotmail.com>

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -185,6 +185,7 @@ impl PixiControl {
                 path: self.project_path().to_path_buf(),
                 channels: None,
                 platforms: Vec::new(),
+                env_file: None,
             },
         }
     }
@@ -197,6 +198,7 @@ impl PixiControl {
                 path: self.project_path().to_path_buf(),
                 channels: None,
                 platforms,
+                env_file: None,
             },
         }
     }
