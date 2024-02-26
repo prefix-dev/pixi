f850c59a9de0290f48e8b523fff2c77b754edaca
Chawye Hsu <su+git@chawyehsu.com>
2024-02-26 17:03:03 +0800

    feat(cli): new `pixi global list` display format (#723)
    Co-authored-by: Ruben Arts <ruben.arts@hotmail.com>
    Co-authored-by: Ruben Arts <ruben@prefix.dev>

--- src/cli/global/install.rs
+++ src/cli/global/install.rs
@@ -70,7 +70,7 @@ impl BinDir {
 }
 
 /// Get pixi home directory, default to `$HOME/.pixi`
-fn home_path() -> miette::Result<PathBuf> {
+pub fn home_path() -> miette::Result<PathBuf> {
     if let Some(path) = std::env::var_os("PIXI_HOME") {
         Ok(PathBuf::from(path))
     } else {
