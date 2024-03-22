e333a90a8106f44e397bdd610346a38dd6f54f8c
Ruben Arts <ruben.arts@hotmail.com>
2024-03-22 13:51:06 +0100

    test: add pytest to the source dep example and add it to ci

---
+++ examples/pypi-source-deps/test/test_imports.py
@@ -0,0 +1,16 @@
+from importlib.metadata import version
+
+def test_flask():
+    assert version("flask") == "3.0.2"
+
+def test_rich():
+    assert version("rich").split(".")[0] == "13"
+
+def test_requests():
+    assert version("requests") == "2.31.0"
+
+def test_minimal_project():
+    assert version("minimal_project") == "0.1"
+
+def test_click():
+    assert version("click") == "8.1.7"
--- tests/test_examples.sh
+++ tests/test_examples.sh
@@ -1,3 +1,4 @@
+# Run from the root of the project using `bash tests/test_examples.sh`
 echo "Running test_examples.sh"
 
 echo "Running the polarify example:"
@@ -6,6 +7,7 @@ pixi run -v --manifest-path examples/polarify/pixi.toml -e pl020 test
 
 echo "Running the pypi example:"
 pixi run -v --manifest-path examples/pypi/pixi.toml test
+pixi run -v --manifest-path examples/pypi-source-deps/pixi.toml test
 
 echo "Running the solve-groups example:"
 pixi run -v --manifest-path examples/solve-groups/pixi.toml -e min-py38 test
