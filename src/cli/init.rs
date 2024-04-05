05650ee3ec0e4eb1dd7b135b94a9e705362deaee
Olivier Lacroix <olivier.lacroix@m4x.org>
2024-04-05 13:48:47 +1100

    Add a message when not runnning init

--- src/cli/init.rs
+++ src/cli/init.rs
@@ -158,52 +158,59 @@ pub async fn execute(args: Args) -> miette::Result<()> {
         // Inject a tool.pixi.project section into an existing pyproject.toml file if there is one without '[tool.pixi.project]'
         if pyproject_manifest_path.is_file() {
             let file = fs::read_to_string(pyproject_manifest_path.clone()).unwrap();
-            if !file.contains("[tool.pixi.project]") {
-                let pyproject = pyproject::pyproject(&file)?;
-                let name = pyproject.project.as_ref().unwrap().name.clone();
-                let environments = pyproject::environments_from_extras(&pyproject);
-                let rv = env
-                    .render_named_str(
-                        consts::PYPROJECT_MANIFEST,
-                        PYROJECT_TEMPLATE,
-                        context! {
-                            name,
-                            channels,
-                            platforms,
-                            environments,
-                        },
-                    )
-                    .unwrap();
-                if let Err(e) = {
-                    fs::OpenOptions::new()
-                        .append(true)
-                        .open(pyproject_manifest_path.clone())
-                        .and_then(|mut p| p.write_all(rv.as_bytes()))
-                } {
-                    tracing::warn!(
-                        "Warning, couldn't update '{}' because of: {}",
-                        pyproject_manifest_path.to_string_lossy(),
-                        e
-                    );
-                } else {
-                    // Inform about the addition of the package itself as an editable dependency of the project
+            if file.contains("[tool.pixi.project]") {
+                eprintln!(
+                    "{}Nothing to do here: 'pyproject.toml' already contains a '[tool.pixi.project]' section.",
+                    console::style(console::Emoji("🤔 ", "")).blue(),
+                );
+                return Ok(());
+            }
+
+            let pyproject = pyproject::pyproject(&file)?;
+            let name = pyproject.project.as_ref().unwrap().name.clone();
+            let environments = pyproject::environments_from_extras(&pyproject);
+            let rv = env
+                .render_named_str(
+                    consts::PYPROJECT_MANIFEST,
+                    PYROJECT_TEMPLATE,
+                    context! {
+                        name,
+                        channels,
+                        platforms,
+                        environments,
+                    },
+                )
+                .unwrap();
+            if let Err(e) = {
+                fs::OpenOptions::new()
+                    .append(true)
+                    .open(pyproject_manifest_path.clone())
+                    .and_then(|mut p| p.write_all(rv.as_bytes()))
+            } {
+                tracing::warn!(
+                    "Warning, couldn't update '{}' because of: {}",
+                    pyproject_manifest_path.to_string_lossy(),
+                    e
+                );
+            } else {
+                // Inform about the addition of the package itself as an editable dependency of the project
+                eprintln!(
+                    "{}Added package '{}' as an editable dependency.",
+                    console::style(console::Emoji("✔ ", "")).green(),
+                    name
+                );
+                // Inform about the addition of environments from extras (if any)
+                if !environments.is_empty() {
+                    let envs: Vec<&str> = environments.keys().map(AsRef::as_ref).collect();
                     eprintln!(
-                        "{}Added package '{}' as an editable dependency.",
+                        "{}Added environment{} '{}' from optional extras.",
                         console::style(console::Emoji("✔ ", "")).green(),
-                        name
-                    );
-                    // Inform about the addition of environments from extras (if any)
-                    if !environments.is_empty() {
-                        let envs: Vec<&str> = environments.keys().map(AsRef::as_ref).collect();
-                        eprintln!(
-                            "{}Added environment{} {} from optional extras.",
-                            console::style(console::Emoji("✔ ", "")).green(),
-                            if envs.len() > 1 { "s" } else { "" },
-                            format!("'{}'", envs.join("', '"))
-                        )
-                    }
+                        if envs.len() > 1 { "s" } else { "" },
+                        envs.join("', '")
+                    )
                 }
             }
+
         // Create a 'pixi.toml' manifest
         } else {
             // Check if the 'pixi.toml' file doesn't already exist. We don't want to overwrite it.
