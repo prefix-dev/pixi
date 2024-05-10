cb31aa11e204d59d0b7a84601e3ff5eed3de6601
Ruben Arts <ruben.arts@hotmail.com>
2024-05-10 13:38:31 +0200

    fix: don't error on not editable not path

--- src/project/manifest/python.rs
+++ src/project/manifest/python.rs
@@ -163,9 +163,10 @@ impl PyPiRequirement {
             PyPiRequirement::Path { editable: e, .. } => {
                 *e = Some(editable);
             }
-            _ => {
+            _ if editable => {
                 tracing::error!("Cannot add editable to a non path like dependency.");
             }
+            _ => {}
         }
     }
 }
