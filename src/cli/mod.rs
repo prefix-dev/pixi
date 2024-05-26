451758ed194d12997277f1d7eb2296bb863bce8d
Bas Zalmstra <bas@prefix.dev>
2024-05-26 16:42:18 +0200

    feat: exec subcommand

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -15,6 +15,7 @@ use crate::{progress, progress::global_multi_progress};
 pub mod add;
 pub mod completion;
 pub mod config;
+mod exec;
 pub mod global;
 pub mod has_specs;
 pub mod info;
@@ -96,6 +97,8 @@ pub enum Command {
     // Execution commands
     #[clap(visible_alias = "r")]
     Run(run::Args),
+    #[clap(visible_alias = "x")]
+    Exec(exec::Args),
     #[clap(visible_alias = "s")]
     Shell(shell::Args),
     ShellHook(shell_hook::Args),
@@ -285,6 +288,7 @@ pub async fn execute_command(command: Command) -> miette::Result<()> {
         Command::List(cmd) => list::execute(cmd).await,
         Command::Tree(cmd) => tree::execute(cmd).await,
         Command::Update(cmd) => update::execute(cmd).await,
+        Command::Exec(args) => exec::execute(args).await,
     }
 }
 
