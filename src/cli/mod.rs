9881989ad6a925944202ea92224798a73084c430
Ruben Arts <ruben.arts@hotmail.com>
2024-07-19 17:00:05 +0200

    chore: bump version to 0.26.0

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -39,7 +39,7 @@ pub mod upload;
 #[command(
     version,
     about = "
-Pixi [version 0.25.0] - Developer Workflow and Environment Management for Multi-Platform, Language-Agnostic Projects.
+Pixi [version 0.26.0] - Developer Workflow and Environment Management for Multi-Platform, Language-Agnostic Projects.
 
 Pixi is a versatile developer workflow tool designed to streamline the management of your project's dependencies, tasks, and environments.
 Built on top of the Conda ecosystem, Pixi offers seamless integration with the PyPI ecosystem.
