2b10ab6f984861cbc6bf06d2d0fa955a8a9cb4cb
Tim de Jager <tim@prefix.dev>
2024-07-25 09:47:38 +0200

    feat: set no_index boolean more correctly

--- src/utils/uv/conversions.rs
+++ src/utils/uv/conversions.rs
@@ -46,9 +46,10 @@ pub fn pypi_options_to_index_locations(options: &PypiOptions) -> IndexLocations 
         })
         .unwrap_or_default();
 
-    // We keep the `no_index` to false for now, because I've not seen a use case for it yet
-    // we could change this later if needed
-    IndexLocations::new(index, extra_indexes, flat_indexes, false)
+    // we don't have support for an explicit `no_index` field in the `PypiOptions`
+    // so we only set it if you want to use flat indexes only
+    let no_index = index.is_none() && !flat_indexes.is_empty();
+    IndexLocations::new(index, extra_indexes, flat_indexes, no_index)
 }
 
 /// Convert locked indexes to IndexLocations
@@ -72,7 +73,11 @@ pub fn locked_indexes_to_index_locations(indexes: &rattler_lock::PypiIndexes) ->
         .iter()
         .map(to_flat_index_location)
         .collect::<Vec<_>>();
-    IndexLocations::new(index, extra_indexes, flat_indexes, false)
+
+    // we don't have support for an explicit `no_index` field in the `PypiIndexes`
+    // so we only set it if you want to use flat indexes only
+    let no_index = index.is_none() && !flat_indexes.is_empty();
+    IndexLocations::new(index, extra_indexes, flat_indexes, no_index)
 }
 
 pub fn to_git_reference(rev: &GitRev) -> GitReference {
