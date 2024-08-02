a8eee191acd98e0344263c57f54c606c2db87093
Tim de Jager <tdejager89@gmail.com>
2024-08-02 13:50:15 +0200

    refactor: move config to repodata functions (#1723)
    This continue to make the `project` module entirely independent of
    anything in the main pixi crate.
    
    This makes moving a lot simpler, which allows us to use it in pixi build
    
    I've put the repodata conversion behind a feature, because not
    everything might need it :)

--- src/cli/exec.rs
+++ src/cli/exec.rs
@@ -18,12 +18,12 @@ use rattler_solve::{resolvo::Solver, SolverImpl, SolverTask};
 use rattler_virtual_packages::VirtualPackage;
 use reqwest_middleware::ClientWithMiddleware;
 
-use crate::utils::config::from_pixi_config;
 use crate::{
     prefix::Prefix,
     progress::{await_in_progress, global_multi_progress, wrap_in_progress},
     utils::{reqwest::build_reqwest_clients, PrefixGuard},
 };
+use pixi_config::gateway::from_pixi_config;
 use pixi_config::{self, Config, ConfigCli};
 
 /// Run a command in a temporary environment.
