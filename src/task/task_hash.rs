26ce001d3e9ced2882086e3069bf22cac495dd8b
Julian Hofer <julianhofer@gnome.org>
2024-08-13 11:59:37 +0200

    Fix intra-doc link

--- src/task/task_hash.rs
+++ src/task/task_hash.rs
@@ -103,7 +103,7 @@ impl TaskHash {
     }
 
     /// Computes a single hash for the task.
-    pub(crate) fn computation_hash(&self) -> ComputationHash {
+    pub fn computation_hash(&self) -> ComputationHash {
         let mut hasher = Xxh3::new();
         self.command.hash(&mut hasher);
         self.inputs.hash(&mut hasher);
