2943d8f51bf21b21a982192704b014af5b0ae584
Hofer-Julian <30049909+Hofer-Julian@users.noreply.github.com>
2024-08-21 12:23:12 +0200

    refactor: Make `HasSpecs` implementation more functional (#1863)
    Co-authored-by: Bas Zalmstra <zalmstra.bas@gmail.com>

--- src/cli/has_specs.rs
+++ src/cli/has_specs.rs
@@ -12,27 +12,30 @@ pub(crate) trait HasSpecs {
     fn packages(&self) -> Vec<&str>;
 
     fn specs(&self) -> miette::Result<IndexMap<PackageName, MatchSpec>> {
-        let mut map = IndexMap::with_capacity(self.packages().len());
-        for package in self.packages() {
-            let spec = MatchSpec::from_str(package, ParseStrictness::Lenient).into_diagnostic()?;
-            let name = spec.name.clone().ok_or_else(|| {
-                miette::miette!("could not find package name in MatchSpec {}", spec)
-            })?;
-            map.insert(name, spec);
-        }
-        Ok(map)
+        self.packages()
+            .iter()
+            .map(|package| {
+                let spec =
+                    MatchSpec::from_str(package, ParseStrictness::Lenient).into_diagnostic()?;
+                let name = spec.name.clone().ok_or_else(|| {
+                    miette::miette!("could not find package name in MatchSpec {}", spec)
+                })?;
+                Ok((name, spec))
+            })
+            .collect()
     }
 
     fn pypi_deps(
         &self,
         project: &Project,
     ) -> miette::Result<IndexMap<PyPiPackageName, Requirement>> {
-        let mut map = IndexMap::with_capacity(self.packages().len());
-        for package in self.packages() {
-            let dep = Requirement::parse(package, project.root()).into_diagnostic()?;
-            let name = PyPiPackageName::from_normalized(dep.clone().name);
-            map.insert(name, dep);
-        }
-        Ok(map)
+        self.packages()
+            .iter()
+            .map(|package| {
+                let dep = Requirement::parse(package, project.root()).into_diagnostic()?;
+                let name = PyPiPackageName::from_normalized(dep.clone().name);
+                Ok((name, dep))
+            })
+            .collect()
     }
 }
