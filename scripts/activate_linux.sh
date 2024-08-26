8e469d053cb6a1bb642b140e8dc8e9af70c7b50c
Julian Hofer <julianhofer@gnome.org>
2024-08-26 15:27:02 +0200

    Move to activation script

---
+++ scripts/activate_linux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -Eeuo pipefail
+export CARGO_TARGET_DIR=".pixi/target"
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="clang"
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS="-C link-arg=-fuse-ld=$CONDA_PREFIX/bin/mold"
