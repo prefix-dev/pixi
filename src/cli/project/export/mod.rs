288e900a769de665c41aa1054309a3b4472e3a13
Alex Kerney <abk@mac.com>
2024-09-07 15:12:20 -0400

    feat: Export conda environment.yml
    Builds upon #1873 to add exporting of `environment.yml` files.
    
    Since #1873 provides the conda-lock style matrix of exports, I focused on exporting an environment.yml that matches the manifest.
    
    Tests against a bunch of the different example manifests for the various pypi-options, and I tested locally that at least some of those were installable with micromamba.
    
    Replaces #1427

--- src/cli/project/export/mod.rs
+++ src/cli/project/export/mod.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+pub mod conda_environment;
 pub mod conda_explicit_spec;
 
 use crate::Project;
@@ -20,12 +21,16 @@ pub enum Command {
     /// Export project environment to a conda explicit specification file
     #[clap(visible_alias = "ces")]
     CondaExplicitSpec(conda_explicit_spec::Args),
+    /// Export project environment to a conda environment.yaml file
+    #[clap(visible_alias = "conda")]
+    CondaEnvironment(conda_environment::Args),
 }
 
 pub async fn execute(args: Args) -> miette::Result<()> {
     let project = Project::load_or_else_discover(args.manifest_path.as_deref())?;
     match args.command {
         Command::CondaExplicitSpec(args) => conda_explicit_spec::execute(project, args).await?,
+        Command::CondaEnvironment(args) => conda_environment::execute(project, args).await?,
     };
     Ok(())
 }
