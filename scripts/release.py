15ccbefc5431c1b046fb0160d8f49badc139a0dc
Ruben Arts <ruben.arts@hotmail.com>
2024-09-19 15:10:42 +0200

    fix: update release.py

--- scripts/release.py
+++ scripts/release.py
@@ -79,107 +79,159 @@ atexit.register(print_summary)
 
 
 def main():
+    steps = [
+        "Start release process",
+        "Check main branch and CI status",
+        "Set release version",
+        "Create and switch to release branch",
+        "Bump all versions",
+        "Update changelog",
+        "Lint changes",
+        "Commit changes",
+        "Push changes",
+        "Create and merge release prep PR",
+        "Tag release",
+        "Push tag",
+        "Publish release",
+        "Test release",
+    ]
+
+    colored_print("Select the step to start from:", "yellow")
+    for i, step in enumerate(steps, 1):
+        colored_print(f"{i}. {step}", "yellow")
+
+    while True:
+        try:
+            start_step = int(colored_input("Enter the step number: ", "magenta"))
+            if 1 <= start_step <= len(steps):
+                break
+            else:
+                colored_print("Invalid step number. Please enter a valid number.", "yellow")
+        except ValueError:
+            colored_print("Invalid input. Please enter a number.", "yellow")
+
+    pixi = get_pixi()
     try:
-        colored_print("Making a release of pixi", "yellow")
-        status.append("Started release process")
-        pixi = get_pixi()
+        if start_step <= 1:
+            colored_print("Making a release of pixi", "yellow")
+            status.append("Started release process")
 
-        # Prep
-        colored_input(
-            "Make sure main is up-to-date and CI passes. Press Enter to continue...", "magenta"
-        )
-        status.append("Checked main branch and CI status")
-
-        release_version = get_release_version()
-        os.environ["RELEASE_VERSION"] = release_version
-        status.append(f"Release version set to {release_version}")
-
-        colored_print("\nCreating a new branch for the release...", "yellow")
-        run_command(["git", "checkout", "main"])
-        run_command(["git", "pull", "upstream", "main"])
-        branch = f"bump/prepare-v{release_version}"
-
-        branch_exists = run_command(["git", "branch", "--list", branch], capture_stdout=True)
-        if branch_exists:
-            run_command(["git", "branch", "--delete", branch])
-        run_command(["git", "switch", "--create", branch])
-        status.append(f"Created and switched to branch {branch}")
-
-        colored_print("\nBumping all versions...", "yellow")
-        run_command([pixi, "run", "bump"])
-        status.append("Bumped all versions")
-
-        colored_print("\nUpdating the changelog...", "yellow")
-        run_command([pixi, "run", "bump-changelog"])
-        colored_input(
-            "Don't forget to update the 'Highlights' section in `CHANGELOG.md`. Press Enter to continue...",
-            "magenta",
-        )
-        status.append("Updated the changelog")
-
-        colored_print("\nCommitting the changes...", "yellow")
-        run_command(["git", "commit", "-am", f"chore: version to {release_version}"])
-        status.append("Committed the changes")
-
-        colored_print("\nPushing the changes...", "yellow")
-        run_command(["git", "push", "origin"])
-        status.append("Pushed the changes")
-
-        # Release prep PR
-        colored_print("\nRelease prep PR", "yellow")
-        colored_input(
-            "Create a PR to check off the change with the peers. Press Enter to continue...",
-            "magenta",
-        )
-        colored_input("Merge that PR. Press Enter to continue...", "magenta")
-        status.append("Created and merged the release prep PR")
-
-        # Tagging the release
-        colored_print("\nTagging the release", "yellow")
-        colored_print("\nChecking out main...", "yellow")
-        run_command(["git", "fetch", "upstream"])
-        run_command(["git", "checkout", "upstream/main"])
-
-        colored_print("\nTagging the release...", "yellow")
-        run_command(["git", "tag", f"v{release_version}", "-m", f"Release {release_version}"])
-        status.append(f"Tagged the release with version {release_version}")
-
-        colored_print("\nPushing the tag...", "yellow")
-        run_command(["git", "push", "upstream", f"v{release_version}"])
-        status.append("Pushed the tag")
-
-        # Publishing the release
-        colored_input(
-            "Update the Release which has CI created for you (after the first build) and add the changelog to the release notes. Press Enter to continue...",
-            "magenta",
-        )
-        colored_input(
-            "Make sure all the artifacts are there and the CI is green!!! Press Enter to continue...",
-            "magenta",
-        )
-        colored_input(
-            "Publish the release and make sure it is set as latest. Press Enter to continue...",
-            "magenta",
-        )
-        status.append("Published the release")
-
-        # Test the release using the install script
-        colored_print("Testing the release using `pixi self-update`...", "yellow")
-        run_command([pixi, "self-update"])
-
-        version_output = run_command([pixi, "--version"], capture_stdout=True)
-        expected_version_output = f"pixi {release_version}"
-        if version_output == expected_version_output:
-            colored_print(f"Version check passed: {version_output}", "yellow")
-        else:
-            colored_print(
-                f"Version check failed: expected {expected_version_output}, got {version_output}",
-                "yellow",
+        if start_step <= 2:
+            colored_input(
+                "Make sure main is up-to-date and CI passes. Press Enter to continue...", "magenta"
             )
-        status.append("Tested the release")
+            status.append("Checked main branch and CI status")
 
-        colored_print("\nDONE!", "yellow")
-        status.append("Release process completed successfully")
+        if start_step <= 3:
+            release_version = get_release_version()
+            os.environ["RELEASE_VERSION"] = release_version
+            status.append(f"Release version set to {release_version}")
+
+        if start_step <= 4:
+            colored_print("\nCreating a new branch for the release...", "yellow")
+            run_command(["git", "checkout", "main"])
+            run_command(["git", "pull", "upstream", "main"])
+            branch = f"bump/prepare-v{release_version}"
+
+            branch_exists = run_command(["git", "branch", "--list", branch], capture_stdout=True)
+            if branch_exists:
+                run_command(["git", "branch", "--delete", branch])
+            run_command(["git", "switch", "--create", branch])
+            status.append(f"Created and switched to branch {branch}")
+
+        if start_step <= 5:
+            colored_print("\nBumping all versions...", "yellow")
+            run_command([pixi, "run", "bump"])
+            status.append("Bumped all versions")
+
+        if start_step <= 6:
+            while True:
+                response = (
+                    colored_input("Should we bump the changelog? (yes/no): ", "magenta")
+                    .strip()
+                    .lower()
+                )
+                if response.lower() in ["yes", "no", "y", "n"]:
+                    break
+                else:
+                    colored_print("Invalid response. Please enter 'yes' or 'no'.", "yellow")
+            if response == "yes" or response == "y":
+                run_command([pixi, "run", "bump-changelog"])
+            colored_input(
+                "Don't forget to update the 'Highlights' section in `CHANGELOG.md`. Press Enter to continue...",
+                "magenta",
+            )
+            status.append("Updated the changelog")
+
+        if start_step <= 7:
+            colored_print("\nLinting the changes...", "yellow")
+            run_command([pixi, "run", "lint"])
+
+        if start_step <= 8:
+            colored_print("\nCommitting the changes...", "yellow")
+            run_command(["git", "commit", "-am", f"chore: version to {release_version}"])
+            status.append("Committed the changes")
+
+        if start_step <= 9:
+            colored_print("\nPushing the changes...", "yellow")
+            run_command(["git", "push", "origin"])
+            status.append("Pushed the changes")
+
+        if start_step <= 10:
+            colored_print("\nRelease prep PR", "yellow")
+            colored_input(
+                "Create a PR to check off the change with the peers. Press Enter to continue...",
+                "magenta",
+            )
+            colored_input("Merge that PR. Press Enter to continue...", "magenta")
+            status.append("Created and merged the release prep PR")
+
+        if start_step <= 11:
+            colored_print("\nTagging the release", "yellow")
+            colored_print("\nChecking out main...", "yellow")
+            run_command(["git", "fetch", "upstream"])
+            run_command(["git", "checkout", "upstream/main"])
+
+            colored_print("\nTagging the release...", "yellow")
+            run_command(["git", "tag", f"v{release_version}", "-m", f"Release {release_version}"])
+            status.append(f"Tagged the release with version {release_version}")
+
+        if start_step <= 12:
+            colored_print("\nPushing the tag...", "yellow")
+            run_command(["git", "push", "upstream", f"v{release_version}"])
+            status.append("Pushed the tag")
+
+        if start_step <= 13:
+            colored_input(
+                "Update the Release which has CI created for you (after the first build) and add the changelog to the release notes. Press Enter to continue...",
+                "magenta",
+            )
+            colored_input(
+                "Make sure all the artifacts are there and the CI is green!!! Press Enter to continue...",
+                "magenta",
+            )
+            colored_input(
+                "Publish the release and make sure it is set as latest. Press Enter to continue...",
+                "magenta",
+            )
+            status.append("Published the release")
+
+            colored_print("Testing the release using `pixi self-update`...", "yellow")
+            run_command([pixi, "self-update"])
+
+            version_output = run_command([pixi, "--version"], capture_stdout=True)
+            expected_version_output = f"pixi {release_version}"
+            if version_output == expected_version_output:
+                colored_print(f"Version check passed: {version_output}", "yellow")
+            else:
+                colored_print(
+                    f"Version check failed: expected {expected_version_output}, got {version_output}",
+                    "yellow",
+                )
+            status.append("Tested the release")
+
+            colored_print("\nDONE!", "yellow")
+            status.append("Release process completed successfully")
 
     except KeyboardInterrupt:
         colored_print("\nProcess interrupted.", "yellow")
