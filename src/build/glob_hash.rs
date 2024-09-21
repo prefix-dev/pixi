288a505acf810ab81669be310fe671f4d30e610b
Bas Zalmstra <zalmstra.bas@gmail.com>
2024-09-21 13:30:11 +0200

    fix: normalize line endings of text files

--- src/build/glob_hash.rs
+++ src/build/glob_hash.rs
@@ -1,6 +1,7 @@
 use std::{
     fs::File,
     io,
+    io::{BufRead, Read, Write},
     path::{Path, PathBuf},
 };
 
@@ -107,8 +108,8 @@ impl GlobHash {
 
             // Concatenate the contents of the file to the hash.
             File::open(&entry)
-                .and_then(|mut f| std::io::copy(&mut f, &mut hasher))
-                .map_err(|e| GlobHashError::IoError(entry.clone(), e))?;
+                .and_then(|mut file| normalize_line_endings(&mut file, &mut hasher))
+                .map_err(move |e| GlobHashError::IoError(entry, e))?;
         }
         let hash = hasher.finalize();
 
@@ -120,12 +121,87 @@ impl GlobHash {
     }
 }
 
+/// This function copy the contents of the reader to the writer but normalizes
+/// the line endings (e.g. replaces `\r\n` with `\n`) in text files.
+fn normalize_line_endings<R: Read, W: Write>(reader: &mut R, writer: &mut W) -> io::Result<()> {
+    let mut reader = io::BufReader::new(reader);
+
+    // Get the first few bytes of the file and check if there is a `0x0` byte in the
+    // input.
+    let mut buffer = reader.fill_buf()?;
+    if buffer.contains(&0) {
+        // This file is binary, compute the hash varbatim.
+        std::io::copy(&mut reader, writer)?;
+    } else {
+        // Read the contents of the file but ignore any `\r` characters.
+        let mut last_cr_pos = None;
+        let mut offset = 0;
+        while !buffer.is_empty() {
+            match memchr::memchr2(b'\r', b'\n', buffer) {
+                Some(pos) if buffer[pos] == b'\r' => {
+                    if last_cr_pos.is_some() {
+                        // We previously detected a `\r` character but did not encounter a newline
+                        writer.write_all(&[b'\r'])?;
+                    }
+
+                    // Process everything up to the '\r' character. Effectively ignoring it.
+                    writer.write_all(&buffer[..pos])?;
+                    reader.consume(pos + 1);
+                    offset += pos + 1;
+                    last_cr_pos = Some(pos + offset);
+                }
+                Some(pos) => {
+                    // Encountered a newline character. If the last time we encountered the `\r` was
+                    // not the previous character, we have to process the last
+                    // `\r` character.
+                    match last_cr_pos {
+                        Some(last_cr_pos) if last_cr_pos + 1 == pos + offset => {
+                            writer.write_all(&[b'\r'])?;
+                            offset += pos + 1;
+                        }
+                        _ => last_cr_pos = None,
+                    }
+
+                    // Process everything up-to and including the newline character.
+                    writer.write_all(&buffer[..=pos])?;
+                    reader.consume(pos + 1);
+                    offset += pos + 1;
+                }
+                None => {
+                    if last_cr_pos.is_some() {
+                        // We previously detected a `\r` character but did not encounter a newline
+                        writer.write_all(&[b'\r'])?;
+                        last_cr_pos = None;
+                    }
+
+                    // This batch of data does not contain any `\r` or `\n` characters. Process the
+                    // entire chunk.
+                    writer.write_all(buffer)?;
+                    let buffer_len = buffer.len();
+                    reader.consume(buffer_len);
+                    offset += buffer_len
+                }
+            }
+            buffer = reader.fill_buf()?;
+        }
+
+        if last_cr_pos.is_some() {
+            // We detected a `\r` at the end of the input.
+            writer.write_all(&[b'\r'])?;
+        }
+    }
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod test {
     use std::path::Path;
 
     use itertools::Itertools;
     use rstest::*;
+
+    use super::*;
 
     #[fixture]
     pub fn testname() -> String {
@@ -140,7 +216,7 @@ mod test {
     #[case::non_glob(vec!["tests/satisfiability/source-dependency/pixi.toml"])]
     fn test_input_hash(testname: String, #[case] globs: Vec<&str>) {
         let root_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
-        let glob_hash = super::GlobHash::from_patterns(root_dir, globs.iter().copied()).unwrap();
+        let glob_hash = GlobHash::from_patterns(root_dir, globs.iter().copied()).unwrap();
         let snapshot = format!(
             "Globs:\n{}\nHash: {:x}\nMatched files:\n{}",
             globs
@@ -153,5 +229,15 @@ mod test {
                 .format_with("\n", |glob, f| f(&format_args!("- {}", glob)))
         );
         insta::assert_snapshot!(testname, snapshot);
+    }
+
+    #[test]
+    fn test_normalize_line_endings() {
+        let input =
+            "\rHello\r\nWorld\r\nYou are the best\nThere is no-one\r\r \rlike you.\r".repeat(8196);
+        let mut normalized: Vec<u8> = Vec::new();
+        normalize_line_endings(&mut input.as_bytes(), &mut normalized).unwrap();
+        let output = String::from_utf8(normalized).unwrap();
+        assert_eq!(output, input.replace("\r\n", "\n"));
     }
 }
