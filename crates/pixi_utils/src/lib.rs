4d099b69b67af539f328b6a04b1b0cf9f75249c6
Ruben Arts <ruben.arts@hotmail.com>
2024-09-26 14:17:15 +0200

    fix: executable parsing on windows (#2142)

--- crates/pixi_utils/src/lib.rs
+++ crates/pixi_utils/src/lib.rs
@@ -3,4 +3,7 @@ pub mod indicatif;
 mod prefix_guard;
 pub mod reqwest;
 
+mod executable_utils;
+pub use executable_utils::{executable_from_path, strip_executable_extension};
+
 pub use prefix_guard::{PrefixGuard, WriteGuard};
