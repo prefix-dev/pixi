206798f81e0d392b90cb75c173566b7b12be9ea6
Sultan Orazbayev <contact@econpoint.com>
2024-10-07 03:20:49 -0400

    Update model.py

--- schema/model.py
+++ schema/model.py
@@ -227,6 +227,9 @@ class PyPIPathRequirement(_PyPIRequirement):
     subdirectory: NonEmptyStr | None = Field(
         None, description="The subdirectory in the repo, a path from the root of the repo."
     )
+    no_deps: bool = Field(
+        False, description="If `true` (default is `false`), the package will be installed without dependencies (matches `--no-deps` in `pip install`)"
+    )
 
 
 class PyPIUrlRequirement(_PyPIRequirement):
