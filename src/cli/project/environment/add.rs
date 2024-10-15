4b38697c8ccf5a51c3925f36f288e458f8490bc1
Julian Hofer <julianhofer@gnome.org>
2024-10-15 16:09:04 +0200

    Use EnvironmentName directly in clap

--- src/cli/project/environment/add.rs
+++ src/cli/project/environment/add.rs
@@ -1,12 +1,11 @@
 use crate::Project;
 use clap::Parser;
 use pixi_manifest::EnvironmentName;
-use std::str::FromStr;
 
 #[derive(Parser, Debug)]
 pub struct Args {
     /// The name of the environment to add.
-    pub name: String,
+    pub name: EnvironmentName,
 
     /// Features to add to the environment.
     #[arg(short, long = "feature")]
@@ -26,9 +25,7 @@ pub struct Args {
 }
 
 pub async fn execute(mut project: Project, args: Args) -> miette::Result<()> {
-    let environment_exists = project
-        .environment(&EnvironmentName::from_str(args.name.as_str())?)
-        .is_some();
+    let environment_exists = project.environment(&args.name).is_some();
     if environment_exists && !args.force {
         return Err(miette::miette!(
             help = "use --force to overwrite the existing environment",
@@ -39,7 +36,7 @@ pub async fn execute(mut project: Project, args: Args) -> miette::Result<()> {
 
     // Add the platforms to the lock-file
     project.manifest.add_environment(
-        args.name.clone(),
+        args.name.as_str().to_string(),
         args.features,
         args.solve_group,
         args.no_default_feature,
