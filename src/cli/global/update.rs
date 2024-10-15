8a3a4a0a25e1c340fdd616d9e1db9155fc40b61e
nichmor <nmorkotilo@gmail.com>
2024-10-15 15:45:56 +0300

    misc: revert report

--- src/cli/global/update.rs
+++ src/cli/global/update.rs
@@ -2,6 +2,7 @@ use crate::cli::global::revert_environment_after_error;
 use crate::global::{self, StateChanges};
 use crate::global::{EnvironmentName, Project};
 use clap::Parser;
+use fancy_display::FancyDisplay;
 use itertools::Itertools;
 use pixi_config::{Config, ConfigCli};
 use pixi_utils::executable_from_path;
@@ -66,7 +67,11 @@ pub async fn execute(args: Args) -> miette::Result<()> {
         // Remove outdated binaries
         state_changes |= project.prune_exposed(env_name).await?;
 
-        state_changes.report();
+        eprintln!(
+            "{}Updated environment: {}.",
+            console::style(console::Emoji("✔ ", "")).green(),
+            env_name.fancy_display()
+        );
 
         Ok(state_changes)
     }
