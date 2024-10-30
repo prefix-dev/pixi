05bcd11f058773c1b4611b829472fc780c9d9f60
Julian Hofer <julianhofer@gnome.org>
2024-10-30 09:41:24 +0100

    Apply Nichita's suggestion

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import Sequence
 
 import pytest
 
@@ -11,21 +10,6 @@ def pytest_addoption(parser: pytest.Parser) -> None:
         default="release",
         help="Specify the pixi build type (e.g., release or debug)",
     )
-    parser.addoption("--runslow", action="store_true", default=False, help="run slow tests")
-
-
-def pytest_configure(config: pytest.Config) -> None:
-    config.addinivalue_line("markers", "slow: mark test as slow to run")
-
-
-def pytest_collection_modifyitems(config: pytest.Config, items: Sequence[pytest.Item]) -> None:
-    if config.getoption("--runslow"):
-        # --runslow given in cli: do not skip slow tests
-        return
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
-    for item in items:
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
 
 
 @pytest.fixture
