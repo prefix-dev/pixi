6c03dbfa0a2365dfa0d78641f1677a0d1428f300
Tim de Jager <tim@prefix.dev>
2024-11-18 16:41:35 +0100

    fix: incorrectly saving absolute base as path component

--- tests/integration_rust/pypi_tests.rs
+++ tests/integration_rust/pypi_tests.rs
@@ -8,7 +8,7 @@ use url::Url;
 #[cfg_attr(not(feature = "slow_integration_tests"), ignore)]
 async fn test_index_strategy() {
     let pypi_indexes = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/data/pypi-indexes");
-    let pypi_indexes_url = Url::from_directory_path(pypi_indexes).unwrap();
+    let pypi_indexes_url = Url::from_directory_path(pypi_indexes.clone()).unwrap();
 
     let pixi = PixiControl::from_manifest(&format!(
         r#"
@@ -68,6 +68,18 @@ async fn test_index_strategy() {
             "foo"
         ),
         Some("1.0.0".into())
+    );
+    // This assertion is specifically to test that if we have a url-based *local* index
+    // we will get a path back to the index
+    assert_eq!(
+        lock_file
+            .get_pypi_package_url("default", Platform::current(), "foo")
+            .unwrap()
+            .as_path()
+            .unwrap(),
+        pypi_indexes
+            .join("multiple-indexes-a/index/foo")
+            .join("foo-1.0.0-py2.py3-none-any.whl")
     );
     assert_eq!(
         lock_file.get_pypi_package_version(
