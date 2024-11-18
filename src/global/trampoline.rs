7b1d336e270a832eeab6559a7cd8cc14d6a54e71
Ruben Arts <ruben.arts@hotmail.com>
2024-11-18 14:35:01 +0100

    fix: typo

--- src/global/trampoline.rs
+++ src/global/trampoline.rs
@@ -67,7 +67,7 @@ const TRAMPOLINE_BIN: &[u8] = include_bytes!(
 #[cfg(target_arch = "powerpc64")]
 #[cfg(target_os = "linux")]
 const TRAMPOLINE_BIN: &[u8] = include_bytes!(
-    "../../crates/pixi_trampoline/trampolines/pixi-trampoline-powerpc64le-unknown-linux-gnu.zst"
+    "../../crates/pixi_trampoline/trampolines/pixi-trampoline-powerpc64-unknown-linux-gnu.zst"
 );
 
 #[cfg(target_arch = "x86_64")]
