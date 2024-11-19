5ed3a2ebb47de9a887b537f165ce40bd6fc805b3
Tim de Jager <tim@prefix.dev>
2024-11-19 11:54:13 +0100

    fix: add integration test for --find-links explicitly

--- tests/integration_rust/pypi_tests.rs
+++ tests/integration_rust/pypi_tests.rs
@@ -6,6 +6,45 @@ use url::Url;
 
 #[tokio::test]
 #[cfg_attr(not(feature = "slow_integration_tests"), ignore)]
+async fn test_flat_links_based_index_returns_path() {
+    let pypi_indexes = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/data/pypi-indexes");
+    let pixi = PixiControl::from_manifest(&format!(
+        r#"
+        [project]
+        name = "pypi-extra-index-url"
+        platforms = ["{platform}"]
+        channels = ["conda-forge"]
+
+        [dependencies]
+        python = "~=3.12.0"
+
+        [pypi-dependencies]
+        foo = "*"
+
+        [pypi-options]
+        find-links = [{{ path = "{pypi_indexes}/multiple-indexes-a/flat"}}]"#,
+        platform = Platform::current(),
+        pypi_indexes = pypi_indexes.display(),
+    ));
+    let lock_file = pixi.unwrap().update_lock_file().await.unwrap();
+
+    // This assertion is specifically to test that if we have a url-based *local* index
+    // we will get a path back to the index and the corresponding file
+    assert_eq!(
+        lock_file
+            .get_pypi_package_url("default", Platform::current(), "foo")
+            .unwrap()
+            .as_path()
+            .unwrap(),
+        pypi_indexes
+            .join("multiple-indexes-a")
+            .join("flat")
+            .join("foo-1.0.0-py2.py3-none-any.whl")
+    );
+}
+
+#[tokio::test]
+#[cfg_attr(not(feature = "slow_integration_tests"), ignore)]
 async fn test_file_based_index_returns_path() {
     let pypi_indexes = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/data/pypi-indexes");
     let pypi_indexes_url = Url::from_directory_path(pypi_indexes.clone()).unwrap();
