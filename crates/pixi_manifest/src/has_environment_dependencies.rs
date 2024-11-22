04daec0009cbfe2b02bbe772258a4f64e8299fc3
Bas Zalmstra <bas@prefix.dev>
2024-11-22 16:45:13 +0100

    fix: code issues

--- crates/pixi_manifest/src/has_environment_dependencies.rs
+++ crates/pixi_manifest/src/has_environment_dependencies.rs
@@ -3,6 +3,7 @@ use rattler_conda_types::Platform;
 use crate::{CondaDependencies, FeaturesExt, HasManifestRef};
 
 /// A trait that defines the dependencies of an environment.
+/// TODO: With the introduction of the `[package]` section this behavior is no longer needed.
 pub trait HasEnvironmentDependencies<'source>:
     HasManifestRef<'source> + FeaturesExt<'source>
 {
@@ -10,8 +11,7 @@ pub trait HasEnvironmentDependencies<'source>:
     /// specific platform.
     ///
     /// The dependencies returned from this function can be either the combined
-    /// (run, host, build) dependencies or only the run dependencies. Which is
-    /// returned is defined by the [`Self::should_combine_dependencies`] method.
+    /// (run, host, build) dependencies or only the run dependencies.
     ///
     /// If the `platform` is `None` no platform specific dependencies are taken
     /// into consideration.
