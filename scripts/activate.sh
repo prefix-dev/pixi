70673f24db438fdcf5b3bf80e8bc22621be406b3
Ruben Arts <ruben.arts@hotmail.com>
2024-11-27 17:04:50 +0100

    fmt

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -2,7 +2,6 @@
 set -Eeuo pipefail
 export CARGO_TARGET_DIR="target/pixi"
 
-# on linux we need to set these rust flags
 export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="clang"
 export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS="-C link-arg=-fuse-ld=$CONDA_PREFIX/bin/mold"
 
