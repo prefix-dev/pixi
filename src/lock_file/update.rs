7a9a850937d06c5631a8325c3fc12ae68912bb38
Julian Hofer <julianhofer@gnome.org>
2024-12-05 11:27:57 +0100

    Only use environment hash if there are no source dependencies

--- src/lock_file/update.rs
+++ src/lock_file/update.rs
@@ -187,11 +187,21 @@ impl<'p> LockFileDerivedData<'p> {
         if update_mode == UpdateMode::QuickValidate {
             if let Ok(Some(environment_file)) = read_environment_file(&environment.dir()) {
                 if environment_file.environment_lock_file_hash == hash {
-                    tracing::info!(
-                        "Environment '{}' is up-to-date with lock file hash",
-                        environment.name().fancy_display()
-                    );
-                    return Ok(Prefix::new(environment.dir()));
+                    let contains_source_packages = self.lock_file.environments().any(|(_, env)| {
+                        env.conda_packages(Platform::current())
+                            .map_or(false, |mut packages| {
+                                packages.any(|package| package.as_source().is_some())
+                            })
+                    });
+                    if contains_source_packages {
+                        tracing::debug!("Lock file contains source packages: ignore lock file hash and update the prefix");
+                    } else {
+                        tracing::info!(
+                            "Environment '{}' is up-to-date with lock file hash",
+                            environment.name().fancy_display()
+                        );
+                        return Ok(Prefix::new(environment.dir()));
+                    }
                 }
             } else {
                 tracing::debug!(
