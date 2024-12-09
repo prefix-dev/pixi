96a8fe77ae4e44cf79e25e6bb915f2e3d70588c7
Liam Connors <connorsl@tcd.ie>
2024-12-08 21:24:36 -0500

    use ProjectConfig

--- src/cli/project/name/mod.rs
+++ src/cli/project/name/mod.rs
@@ -1,16 +1,15 @@
 pub mod get;
 pub mod set;
 
+use crate::cli::cli_config::ProjectConfig;
 use crate::Project;
 use clap::Parser;
-use std::path::PathBuf;
 
 /// Commands to manage project name.
 #[derive(Parser, Debug)]
 pub struct Args {
-    /// The path to `pixi.toml` or `pyproject.toml`
-    #[clap(long, global = true)]
-    pub manifest_path: Option<PathBuf>,
+    #[clap(flatten)]
+    pub project_config: ProjectConfig,
 
     /// The subcommand to execute
     #[clap(subcommand)]
@@ -26,7 +25,7 @@ pub enum Command {
 }
 
 pub async fn execute(args: Args) -> miette::Result<()> {
-    let project = Project::load_or_else_discover(args.manifest_path.as_deref())?;
+    let project = Project::load_or_else_discover(args.project_config.manifest_path.as_deref())?;
 
     match args.command {
         Command::Get => get::execute(project).await?,
