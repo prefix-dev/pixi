853246ee57f7b258d70191fb567d6bf599720a36
Ruben Arts <ruben.arts@hotmail.com>
2024-12-12 11:18:57 +0100

    test: add menu install test

--- tests/integration_python/pixi_global/test_global.py
+++ tests/integration_python/pixi_global/test_global.py
@@ -1821,3 +1821,24 @@ def test_remove_dependency(pixi: Path, tmp_pixi_workspace: Path, dummy_channel_1
         env=env,
         stderr_contains="Environment dummy-a doesn't exist",
     )
+
+def test_menuinst_install(pixi: Path, tmp_pixi_workspace: Path, dummy_channel_1: str) -> None:
+    env = {"PIXI_HOME": str(tmp_pixi_workspace)}
+
+    verify_cli_command(
+        [
+            pixi,
+            "global",
+            "install",
+            "--channel",
+            dummy_channel_1,
+            "pixi-editor",
+        ],
+        env=env,
+        stdout_contains=[
+            "pixi-editor",
+            "menu install: true",
+        ],
+
+        # TODO: Check if there was an actual menu item on the machine.
+    )
