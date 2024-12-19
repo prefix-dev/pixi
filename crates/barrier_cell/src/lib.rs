7f4a45ad4833ff95ec36dfcc8b6ad482271b0f7e
Bas Zalmstra <zalmstra.bas@gmail.com>
2024-12-19 11:47:08 +0100

    fix: race condition in barriercell

--- crates/barrier_cell/src/lib.rs
+++ crates/barrier_cell/src/lib.rs
@@ -1,16 +1,24 @@
-use std::sync::OnceLock;
-use thiserror::Error;
-use tokio::sync::Notify;
+use std::sync::Arc;
 
-/// A synchronization primitive, around [`OnceLock`] that can be used to wait for a value to become available.
+use parking_lot::Mutex;
+use thiserror::Error;
+
+/// A synchronization primitive, around [`OnceLock`] that can be used to wait
+/// for a value to become available.
 ///
-/// The [`BarrierCell`] is initially empty, requesters can wait for a value to become available
-/// using the `wait` method. Once a value is available, the `set` method can be used to set the
-/// value in the cell. The `set` method can only be called once. If the `set` method is called
-/// multiple times, it will return an error. When `set` is called successfully all waiters will be notified.
+/// The [`BarrierCell`] is initially empty, requesters can wait for a value to
+/// become available using the `wait` method. Once a value is available, the
+/// `set` method can be used to set the value in the cell. The `set` method can
+/// only be called once. If the `set` method is called multiple times, it will
+/// return an error. When `set` is called successfully all waiters will be
+/// notified.
 pub struct BarrierCell<T> {
-    value: OnceLock<T>,
-    notify: Notify,
+    data: Mutex<Option<ValueOrNotify<T>>>,
+}
+
+enum ValueOrNotify<T> {
+    Value(Arc<T>),
+    Notify(tokio::sync::broadcast::Sender<Arc<T>>),
 }
 
 impl<T> Default for BarrierCell<T> {
@@ -28,29 +36,52 @@ pub enum SetError {
 impl<T> BarrierCell<T> {
     /// Constructs a new instance.
     pub fn new() -> Self {
+        let (sender, _) = tokio::sync::broadcast::channel(1);
         Self {
-            value: OnceLock::new(),
-            notify: Notify::new(),
+            data: Mutex::new(Some(ValueOrNotify::Notify(sender))),
         }
     }
 
     /// Wait for a value to become available in the cell
-    pub async fn wait(&self) -> &T {
-        self.notify.notified().await;
-        // safe, as notification only occurs after setting the value
-        unsafe { self.value.get().unwrap_unchecked() }
+    #[expect(clippy::await_holding_lock)]
+    pub async fn wait(&self) -> Arc<T> {
+        let lock = self.data.lock();
+        match &*lock {
+            Some(ValueOrNotify::Value(value)) => value.clone(),
+            Some(ValueOrNotify::Notify(notify)) => {
+                let mut recv = notify.subscribe();
+                drop(lock);
+                recv.recv().await.expect("notify channel closed")
+            }
+            _ => unreachable!(),
+        }
     }
 
-    /// Set the value in the cell, if the cell was already initialized this will return an error.
-    pub fn set(&self, value: T) -> Result<(), SetError> {
-        self.value.set(value).map_err(|_| SetError::AlreadySet)?;
-        self.notify.notify_waiters();
-
-        Ok(())
+    /// Set the value in the cell, if the cell was already initialized this will
+    /// return an error.
+    pub fn set(&self, value: Arc<T>) -> Result<(), SetError> {
+        let mut lock = self.data.lock();
+        match lock.take() {
+            Some(ValueOrNotify::Value(value)) => {
+                *lock = Some(ValueOrNotify::Value(value));
+                Err(SetError::AlreadySet)
+            }
+            Some(ValueOrNotify::Notify(notify)) => {
+                *lock = Some(ValueOrNotify::Value(value.clone()));
+                drop(lock);
+                let _ = notify.send(value);
+                Ok(())
+            }
+            _ => unreachable!(),
+        }
     }
 
-    /// Consumes this instance and converts it into the inner value if it has been initialized.
-    pub fn into_inner(self) -> Option<T> {
-        self.value.into_inner()
+    /// Consumes this instance and converts it into the inner value if it has
+    /// been initialized.
+    pub fn into_inner(self) -> Option<Arc<T>> {
+        self.data.into_inner().and_then(|v| match v {
+            ValueOrNotify::Value(value) => Some(value),
+            _ => None,
+        })
     }
 }
--- src/lock_file/update.rs
+++ src/lock_file/update.rs
@@ -469,12 +469,10 @@ pub struct UpdateContext<'p> {
     /// mapping contains a [`BarrierCell`] that will eventually contain the
     /// solved records computed by another task. This allows tasks to wait
     /// for the records to be solved before proceeding.
-    solved_repodata_records:
-        PerEnvironmentAndPlatform<'p, Arc<BarrierCell<Arc<PixiRecordsByName>>>>,
+    solved_repodata_records: PerEnvironmentAndPlatform<'p, Arc<BarrierCell<PixiRecordsByName>>>,
 
     /// Keeps track of all pending grouped conda targets that are being solved.
-    grouped_solved_repodata_records:
-        PerGroupAndPlatform<'p, Arc<BarrierCell<Arc<PixiRecordsByName>>>>,
+    grouped_solved_repodata_records: PerGroupAndPlatform<'p, Arc<BarrierCell<PixiRecordsByName>>>,
 
     /// Keeps track of all pending prefix updates. This only tracks the conda
     /// updates to a prefix, not whether the pypi packages have also been
@@ -485,10 +483,10 @@ pub struct UpdateContext<'p> {
     /// mapping contains a [`BarrierCell`] that will eventually contain the
     /// solved records computed by another task. This allows tasks to wait
     /// for the records to be solved before proceeding.
-    solved_pypi_records: PerEnvironmentAndPlatform<'p, Arc<BarrierCell<Arc<PypiRecordsByName>>>>,
+    solved_pypi_records: PerEnvironmentAndPlatform<'p, Arc<BarrierCell<PypiRecordsByName>>>,
 
     /// Keeps track of all pending grouped pypi targets that are being solved.
-    grouped_solved_pypi_records: PerGroupAndPlatform<'p, Arc<BarrierCell<Arc<PypiRecordsByName>>>>,
+    grouped_solved_pypi_records: PerGroupAndPlatform<'p, Arc<BarrierCell<PypiRecordsByName>>>,
 
     /// The package cache to use when instantiating prefixes.
     package_cache: PackageCache,
@@ -631,7 +629,7 @@ impl<'p> UpdateContext<'p> {
                         .expect("prefixes must not be shared")
                         .into_inner()
                         .expect("prefix must be available");
-                    Some((env.name().clone(), prefix))
+                    Some((env.name().clone(), (prefix.0.clone(), prefix.1.clone())))
                 }
                 _ => None,
             })
@@ -646,7 +644,10 @@ impl<'p> UpdateContext<'p> {
         environment: &GroupedEnvironment<'p>,
     ) -> Option<impl Future<Output = (Prefix, PythonStatus)>> {
         let cell = self.instantiated_conda_prefixes.get(environment)?.clone();
-        Some(async move { cell.wait().await.clone() })
+        Some(async move {
+            let prefix = cell.wait().await;
+            (prefix.0.clone(), prefix.1.clone())
+        })
     }
 }
 
@@ -1428,7 +1429,7 @@ impl<'p> UpdateContext<'p> {
                     self.instantiated_conda_prefixes
                         .get_mut(&group)
                         .expect("the entry for this environment should exists")
-                        .set((prefix, *python_status))
+                        .set(Arc::new((prefix, *python_status)))
                         .expect("prefix should not be instantiated twice");
 
                     tracing::info!(
