f1a0237330337bf5729ab548b60d9a8f8fa5e037
Bas Zalmstra <bas@prefix.dev>
2025-01-03 13:31:13 +0100

    fix: snapshot whitespace characters

--- crates/pixi_manifest/src/utils/test_utils.rs
+++ crates/pixi_manifest/src/utils/test_utils.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use miette::{GraphicalReportHandler, GraphicalTheme, NamedSource, Report};
 
 use crate::toml::FromTomlStr;
@@ -33,5 +34,10 @@ pub(crate) fn format_parse_error(source: &str, error: TomlError) -> String {
                 .as_ref(),
         )
         .unwrap();
-    s
+
+    // Remove trailing whitespace in the error message.
+    s.lines()
+        .map(|line| line.trim_end())
+        .format("\n")
+        .to_string()
 }
