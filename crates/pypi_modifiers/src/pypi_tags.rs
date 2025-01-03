72a6f1975f7f97c3c8868e9e86166f5c1cc47ea1
nichmor <nmorkotilo@gmail.com>
2025-01-03 12:03:33 +0200

    misc: change the implementation

--- crates/pypi_modifiers/src/pypi_tags.rs
+++ crates/pypi_modifiers/src/pypi_tags.rs
@@ -183,7 +183,6 @@ fn gil_disabled(python_record: &PackageRecord) -> miette::Result<bool> {
             .is_some_and(|name| name.as_source() == "python_abi")
             && spec.build.as_ref().is_some_and(|build| {
                 let raw_str = format!("{}", build);
-                dbg!(&raw_str);
                 regex.is_match(&raw_str)
             })
     }))
