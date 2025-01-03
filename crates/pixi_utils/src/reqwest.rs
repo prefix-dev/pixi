045b0d8e3881bdca7499308a647e0e34bd8a118e
Daniel Elsner <daniel.elsner@quantco.com>
2025-01-03 11:02:02 +0100

    Add example for s3 usage

--- crates/pixi_utils/src/reqwest.rs
+++ crates/pixi_utils/src/reqwest.rs
@@ -6,7 +6,7 @@ use rattler_networking::{
     mirror_middleware::Mirror,
     retry_policies::ExponentialBackoff,
     AuthenticationMiddleware, AuthenticationStorage, GCSMiddleware, MirrorMiddleware,
-    OciMiddleware,
+    OciMiddleware, S3Middleware,
 };
 
 use reqwest::Client;
@@ -110,6 +110,7 @@ pub fn build_reqwest_clients(config: Option<&Config>) -> (Client, ClientWithMidd
     }
 
     client_builder = client_builder.with(GCSMiddleware);
+    client_builder = client_builder.with(S3Middleware::new(None, None, Some(true)));
 
     client_builder = client_builder.with_arc(Arc::new(
         auth_middleware(&config).expect("could not create auth middleware"),
