b918f25b4d5c375984c3b9dd12cbc64f7ca3cb44
Bas Zalmstra <bas@prefix.dev>
2025-01-10 11:02:25 +0100

    perf: use faster allocators

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// This forces the crate to be compiled even though the crate is not used in the project.
+// https://github.com/rust-lang/rust/issues/64402
+#[cfg(feature = "pixi_allocator")]
+extern crate pixi_allocator;
+
 #[tokio::main]
 pub async fn main() -> miette::Result<()> {
     pixi::cli::execute().await
