ae63738ae1736842d22aec576363ee286f0d9b72
nichmor <nmorkotilo@gmail.com>
2025-01-15 11:26:44 +0200

    misc: skip test exec on windows

--- tests/integration_python/test_exec.py
+++ tests/integration_python/test_exec.py
@@ -1,8 +1,15 @@
 from pathlib import Path
+import sys
+
+import pytest
 from .common import verify_cli_command
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 
+@pytest.mark.skipif(
+    sys.platform.startswith("win"),
+    reason="For some reason .bat files are not correctly executed on windows",
+)
 def test_concurrent_exec(pixi: Path, dummy_channel_1: str) -> None:
     with ProcessPoolExecutor(max_workers=2) as executor:
         # Run the two exact same tasks in parallel
