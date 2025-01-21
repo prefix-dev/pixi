83e05e638ac17f2cc041097e94cf4d5eb2b5a612
nichmor <nmorkotilo@gmail.com>
2025-01-21 13:02:17 +0200

    misc: fix rustdoc

--- crates/pixi_uv_conversions/src/conversions.rs
+++ crates/pixi_uv_conversions/src/conversions.rs
@@ -242,10 +242,10 @@ pub fn into_pinned_git_spec(dist: GitSourceDist) -> PinnedGitSpec {
 
 /// Convert a locked git url into a parsed git url
 /// [`LockedGitUrl`] is always recorded in the lock file and looks like this:
-/// git+https://git.example.com/MyProject.git?tag=v1.0&subdirectory=pkg_dir#1c4b2c7864a60ea169e091901fcde63a8d6fbfdc
+/// <git+https://git.example.com/MyProject.git?tag=v1.0&subdirectory=pkg_dir#1c4b2c7864a60ea169e091901fcde63a8d6fbfdc>
 ///
 /// [`ParsedGitUrl`] looks like this:
-/// git+https://git.example.com/MyProject.git@v1.0#subdirectory=pkg_dir
+/// <git+https://git.example.com/MyProject.git@v1.0#subdirectory=pkg_dir>
 ///
 /// So we need to convert the locked git url into a parsed git url.
 /// which is used in the uv crate.
