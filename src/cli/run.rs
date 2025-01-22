29931f2d21706713936d3d407defeb39822db403
Julian Hofer <julianhofer@gnome.org>
2025-01-22 16:16:50 +0100

    Fix typo

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -302,7 +302,7 @@ async fn execute_task<'p>(
 
     // Ignore CTRL+C
     // Specifically so that the child is responsible for its own signal handling
-    // NOTE: one CTRL+C is registered it will always stay registered for the rest of
+    // NOTE: once CTRL+C is registered it will always stay registered for the rest of
     // the runtime of the program which is fine when using run in isolation,
     // however if we start to use run in conjunction with some other command we
     // might want to revaluate this.
