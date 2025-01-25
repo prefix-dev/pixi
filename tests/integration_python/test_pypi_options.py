5aca7bf427cb26f3ee0e83bfe1be26a96fe2c487
Tim de Jager <tim@prefix.dev>
2025-01-25 14:04:40 +0100

    feat: added integration test

---
+++ tests/integration_python/test_pypi_options.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import pytest
+from .common import verify_cli_command, ExitCode
+
+
+@pytest.mark.extra_slow
+def test_no_build_option(pixi: Path, tmp_pixi_workspace: Path) -> None:
+    """
+    Tests the behavior of pixi install command when the no-build option is specified in pixi.toml.
+    This test verifies that the installation fails appropriately when attempting to install
+    packages that need to be built like `sdist`.
+    """
+    test_data = Path(__file__).parent.parent / "data/pixi_tomls/no_build.toml"
+    manifest = tmp_pixi_workspace.joinpath("pixi.toml")
+    toml = test_data.read_text()
+    manifest.write_text(toml)
+
+    # Run the installation
+    verify_cli_command(
+        [pixi, "install", "--manifest-path", manifest],
+        ExitCode.FAILURE,
+    )
