e638a486b0158776815008bce26a67b6b2196281
Julian Hofer <julianhofer@gnome.org>
2025-02-14 17:29:07 +0100

    Add more tests

--- tests/integration_python/pixi_build/conftest.py
+++ tests/integration_python/pixi_build/conftest.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import pytest
+from ..common import CURRENT_PLATFORM
 
 
 @pytest.fixture
@@ -16,3 +17,38 @@ def examples_dir() -> Path:
     Returns the path to the examples directory in the root of the repository
     """
     return Path(__file__).parents[3].joinpath("examples").resolve()
+
+
+@pytest.fixture
+def simple_workspace(tmp_pixi_workspace: Path, request: pytest.FixtureRequest) -> Path:
+    name = request.node.name
+
+    recipe = f"""
+    package:
+      name: {name}
+      version: 1.0.0
+    """
+    recipe_path = tmp_pixi_workspace.joinpath("recipe.yaml")
+    recipe_path.write_text(recipe)
+
+    manifest = f"""
+    [workspace]
+    channels = ["https://prefix.dev/pixi-build-backends", "https://prefix.dev/conda-forge"]
+    preview = ["pixi-build"]
+    platforms = ["{CURRENT_PLATFORM}"]
+    name = "{name}"
+    version = "1.0.0"
+
+    [dependencies]
+    {name} = {{ path = "." }}
+
+    [package.build.backend]
+    name = "pixi-build-rattler-build"
+    version = "0.1.*"
+
+    [package.build.configuration]
+    debug-dir = "{tmp_pixi_workspace}"
+    """
+    manifest_path = tmp_pixi_workspace.joinpath("pixi.toml")
+    manifest_path.write_text(manifest)
+    return manifest_path
--- tests/integration_python/pixi_build/test_build.py
+++ tests/integration_python/pixi_build/test_build.py
@@ -4,27 +4,28 @@ import json
 import pytest
 
 from ..common import ExitCode, verify_cli_command
+import tomllib
+import tomli_w
 
 
 @pytest.mark.slow
-def test_build_conda_package(pixi: Path, tmp_pixi_workspace: Path, build_data: Path) -> None:
-    """
-    This one tries to build the rich example project
-    """
-
-    project = build_data / "rich_example"
-    shutil.rmtree(project.joinpath(".pixi"), ignore_errors=True)
-    shutil.copytree(project, tmp_pixi_workspace, dirs_exist_ok=True)
-
-    manifest_path = tmp_pixi_workspace / "pixi.toml"
-
-    # build it
+def test_build_conda_package(
+    pixi: Path,
+    simple_workspace: Path,
+) -> None:
     verify_cli_command(
-        [pixi, "build", "--manifest-path", manifest_path, "--output-dir", manifest_path.parent]
+        [
+            pixi,
+            "build",
+            "--manifest-path",
+            simple_workspace,
+            "--output-dir",
+            simple_workspace.parent,
+        ],
     )
 
     # really make sure that conda package was built
-    built_packages = list(manifest_path.parent.glob("*.conda"))
+    built_packages = list(simple_workspace.parent.glob("*.conda"))
 
     assert len(built_packages) == 1
     assert built_packages[0].exists()
@@ -117,6 +118,52 @@ def test_source_change_trigger_rebuild(
         ],
         stdout_contains="The version of simple-pyproject is 2.0.0",
     )
+
+
+def test_host_dependency_change_trigger_rebuild(
+    pixi: Path, simple_workspace: Path, dummy_channel_1: Path
+) -> None:
+    # Setting PIXI_CACHE_DIR shouldn't be necessary
+    env = {
+        "PIXI_BUILD_BACKEND_OVERRIDE": "pixi-build-rattler-build=/var/home/julian/Projekte/github.com/prefix-dev/pixi-build-backends/target/release/pixi-build-rattler-build",
+        "PIXI_CACHE_DIR": str(simple_workspace.parent.joinpath(".cache")),
+    }
+    verify_cli_command(
+        [
+            pixi,
+            "install",
+            "--manifest-path",
+            simple_workspace,
+        ],
+        env=env,
+    )
+
+    conda_build_params = simple_workspace.parent.joinpath("conda_build_params.json")
+
+    assert conda_build_params.is_file()
+
+    # Remove the conda build params to get a clean state
+    conda_build_params.unlink()
+
+    # Add dummy-b to host-dependencies
+    manifest_data = tomllib.loads(simple_workspace.read_text())
+    manifest_data["package"].setdefault("host-dependencies", {})["dummy-b"] = {
+        "version": "*",
+        "channel": dummy_channel_1,
+    }
+    simple_workspace.write_text(tomli_w.dumps(manifest_data))
+    verify_cli_command(
+        [
+            pixi,
+            "install",
+            "--manifest-path",
+            simple_workspace,
+        ],
+        env=env,
+    )
+
+    # modifying the host-dependencies should trigger a rebuild and therefore create a file
+    assert conda_build_params.is_file()
 
 
 @pytest.mark.slow
