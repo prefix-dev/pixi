7b9236ee0dba25b2c6fe537aa6038b76f6ddc082
Hofer-Julian <30049909+Hofer-Julian@users.noreply.github.com>
2025-03-10 15:36:48 +0100

    fix: `pixi global install --force-reinstall` (#3307)

--- src/cli/global/install.rs
+++ src/cli/global/install.rs
@@ -65,7 +65,7 @@ pub struct Args {
     #[clap(flatten)]
     config: ConfigCli,
 
-    /// Specifies that the packages should be reinstalled even if they are already installed.
+    /// Specifies that the environment should be reinstalled.
     #[arg(action, long)]
     force_reinstall: bool,
 
@@ -168,6 +168,10 @@ async fn setup_environment(
 ) -> miette::Result<StateChanges> {
     let mut state_changes = StateChanges::new_with_env(env_name.clone());
 
+    if args.force_reinstall && project.environment(env_name).is_some() {
+        state_changes |= project.remove_environment(env_name).await?;
+    }
+
     let channels = if args.channels.is_empty() {
         project.config().default_channels()
     } else {
@@ -207,7 +211,7 @@ async fn setup_environment(
         }
     }
 
-    if !args.force_reinstall && project.environment_in_sync(env_name).await? {
+    if project.environment_in_sync(env_name).await? {
         return Ok(StateChanges::new_with_env(env_name.clone()));
     }
 
