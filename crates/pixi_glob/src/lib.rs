dd55fb61288e5b8856dfd1fd5eccab0f3b1ae7ac
Parsa Bahraminejad <p.bahraminejad@gmail.com>
2025-03-12 07:28:49 -0400

    refactor: replace ignore crate with pixi_glob (#3333)

--- crates/pixi_glob/src/lib.rs
+++ crates/pixi_glob/src/lib.rs
@@ -10,3 +10,4 @@ mod glob_set;
 pub use glob_hash::{GlobHash, GlobHashError};
 pub use glob_hash_cache::{GlobHashCache, GlobHashKey};
 pub use glob_mtime::{GlobModificationTime, GlobModificationTimeError};
+pub use glob_set::{GlobSet, GlobSetError};
