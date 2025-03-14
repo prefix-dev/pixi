9b13afd18c0accb762bd71192b44d5cba21d5cb9
Julian Hofer <julianhofer@gnome.org>
2025-03-14 14:40:15 +0100

    Finalize tests

--- tests/integration_python/test_reinstall.py
+++ tests/integration_python/test_reinstall.py
@@ -13,10 +13,13 @@ def reinstall_workspace(tmp_pixi_workspace: Path, mock_projects: Path) -> Path:
     shutil.copytree(test_rebuild_src, tmp_pixi_workspace, dirs_exist_ok=True)
 
     # Enable debug logging
-    package_manifest = tmp_pixi_workspace.joinpath("pixi_build_package", "pixi.toml")
-    manifest_dict = tomllib.loads(package_manifest.read_text())
-    manifest_dict["package"]["build"]["configuration"] = {"debug-dir": str(tmp_pixi_workspace)}
-    package_manifest.write_text(tomli_w.dumps(manifest_dict))
+    packages = ["pixi_build_package", "pixi_build_package_dev"]
+    for package in packages:
+        package_dir = tmp_pixi_workspace / package
+        package_manifest = package_dir / "pixi.toml"
+        manifest_dict = tomllib.loads(package_manifest.read_text())
+        manifest_dict["package"]["build"]["configuration"] = {"debug-dir": str(package_dir)}
+        package_manifest.write_text(tomli_w.dumps(manifest_dict))
 
     return tmp_pixi_workspace
 
@@ -27,14 +30,12 @@ def test_pixi_reinstall_default_env(pixi: Path, reinstall_workspace: Path) -> No
         "PIXI_CACHE_DIR": str(reinstall_workspace.joinpath("pixi_cache")),
     }
     manifest = reinstall_workspace.joinpath("pixi.toml")
-    pypi_package_init = reinstall_workspace.joinpath(
-        "pypi_package", "src", "pypi_package", "__init__.py"
+    conda_metadata_params = reinstall_workspace.joinpath(
+        "pixi_build_package", "conda_metadata_params.json"
     )
-    pixi_build_package_init = reinstall_workspace.joinpath(
-        "pixi_build_package", "src", "pixi_build_package", "__init__.py"
+    conda_build_params = reinstall_workspace.joinpath(
+        "pixi_build_package", "conda_build_params.json"
     )
-    conda_metadata_params = reinstall_workspace.joinpath("conda_metadata_params.json")
-    conda_build_params = reinstall_workspace.joinpath("conda_build_params.json")
 
     # Check that packages return "number 1"
     verify_cli_command(
@@ -57,8 +58,9 @@ def test_pixi_reinstall_default_env(pixi: Path, reinstall_workspace: Path) -> No
     conda_build_params.unlink()
 
     # Modify the Python files
-    pypi_package_init.write_text(pypi_package_init.read_text().replace("1", "2"))
-    pixi_build_package_init.write_text(pixi_build_package_init.read_text().replace("1", "2"))
+    for package in ["pypi_package", "pixi_build_package"]:
+        init_py = reinstall_workspace.joinpath(package, "src", package, "__init__.py")
+        init_py.write_text(init_py.read_text().replace("1", "2"))
 
     # That shouldn't trigger a re-install, so running still returns "number 1"
     verify_cli_command(
@@ -78,7 +80,7 @@ def test_pixi_reinstall_default_env(pixi: Path, reinstall_workspace: Path) -> No
 
     # After re-installing pypi-package, it should return "number 2"
     # pixi-build-package, should not be rebuild and therefore still return "number 1"
-    verify_cli_command([pixi, "reinstall", "--manifest-path", manifest, "pypi-package"], env=env)
+    verify_cli_command([pixi, "reinstall", "--manifest-path", manifest, "pypi_package"], env=env)
     assert not conda_metadata_params.is_file()
     assert not conda_build_params.is_file()
     verify_cli_command(
@@ -115,19 +117,41 @@ def test_pixi_reinstall_multi_env(pixi: Path, reinstall_workspace: Path) -> None
         "PIXI_CACHE_DIR": str(reinstall_workspace.joinpath("pixi_cache")),
     }
     manifest = reinstall_workspace.joinpath("pixi.toml")
-    pypi_package_init = reinstall_workspace.joinpath(
-        "pypi_package", "src", "pypi_package", "__init__.py"
+    conda_metadata_params = reinstall_workspace.joinpath(
+        "pixi_build_package", "conda_metadata_params.json"
     )
-    pixi_build_package_init = reinstall_workspace.joinpath(
-        "pixi_build_package", "src", "pixi_build_package", "__init__.py"
+    conda_build_params = reinstall_workspace.joinpath(
+        "pixi_build_package", "conda_build_params.json"
     )
-    conda_metadata_params = reinstall_workspace.joinpath("conda_metadata_params.json")
-    conda_build_params = reinstall_workspace.joinpath("conda_build_params.json")
+    conda_metadata_params_dev = reinstall_workspace.joinpath(
+        "pixi_build_package_dev", "conda_metadata_params.json"
+    )
+    conda_build_params_dev = reinstall_workspace.joinpath(
+        "pixi_build_package_dev", "conda_build_params.json"
+    )
 
-    # Check that packages return "number 1"
+    # Check that packages return "number 1" in default environment
     verify_cli_command(
-        [pixi, "run", "--manifest-path", manifest, "--environment", "dev", "pypi-package-main"],
+        [pixi, "run", "--manifest-path", manifest, "pypi-package-main"],
         stdout_contains="PyPI is number 1",
+        env=env,
+    )
+    verify_cli_command(
+        [
+            pixi,
+            "run",
+            "--manifest-path",
+            manifest,
+            "pixi-build-package-main",
+        ],
+        stdout_contains="Pixi Build is number 1",
+        env=env,
+    )
+
+    # Check that packages return "number 1" in dev environment
+    verify_cli_command(
+        [pixi, "run", "--manifest-path", manifest, "--environment", "dev", "pypi-package-dev-main"],
+        stdout_contains="PyPI dev is number 1",
         env=env,
     )
     verify_cli_command(
@@ -138,28 +162,34 @@ def test_pixi_reinstall_multi_env(pixi: Path, reinstall_workspace: Path) -> None
             manifest,
             "--environment",
             "dev",
-            "pixi-build-package-main",
+            "pixi-build-package-dev-main",
         ],
-        stdout_contains="Pixi Build is number 1",
+        stdout_contains="Pixi Build dev is number 1",
         env=env,
     )
 
     # In order to build pixi-build-package-main, getMetadata and build has been called
-    assert conda_metadata_params.is_file()
-    assert conda_build_params.is_file()
+    assert conda_metadata_params_dev.is_file()
+    assert conda_build_params_dev.is_file()
 
     # Delete the files to get a clean state
-    conda_metadata_params.unlink()
-    conda_build_params.unlink()
+    conda_metadata_params_dev.unlink()
+    conda_build_params_dev.unlink()
 
     # Modify the Python files
-    pypi_package_init.write_text(pypi_package_init.read_text().replace("1", "2"))
-    pixi_build_package_init.write_text(pixi_build_package_init.read_text().replace("1", "2"))
+    for package in [
+        "pypi_package",
+        "pixi_build_package",
+        "pypi_package_dev",
+        "pixi_build_package_dev",
+    ]:
+        init_py = reinstall_workspace.joinpath(package, "src", package, "__init__.py")
+        init_py.write_text(init_py.read_text().replace("1", "2"))
 
     # That shouldn't trigger a re-install, so running still returns "number 1"
     verify_cli_command(
-        [pixi, "run", "--manifest-path", manifest, "--environment", "dev", "pypi-package-main"],
-        stdout_contains="PyPI is number 1",
+        [pixi, "run", "--manifest-path", manifest, "--environment", "dev", "pypi-package-dev-main"],
+        stdout_contains="PyPI dev is number 1",
         env=env,
     )
     verify_cli_command(
@@ -170,18 +200,18 @@ def test_pixi_reinstall_multi_env(pixi: Path, reinstall_workspace: Path) -> None
             manifest,
             "--environment",
             "dev",
-            "pixi-build-package-main",
+            "pixi-build-package-dev-main",
         ],
-        stdout_contains="Pixi Build is number 1",
+        stdout_contains="Pixi Build dev is number 1",
         env=env,
     )
 
     # Everything pixi-build related is cached, no remote procedure was called
-    assert not conda_metadata_params.is_file()
-    assert not conda_build_params.is_file()
+    assert not conda_metadata_params_dev.is_file()
+    assert not conda_build_params_dev.is_file()
 
-    # After re-building pixi-build-package, it should return "number 2"
-    # pypi-package, should still return "number 1"
+    # After re-building pixi_build_package_dev, it should return "number 2"
+    # pypi_package_dev, should still return "number 1"
     verify_cli_command(
         [
             pixi,
@@ -190,12 +220,12 @@ def test_pixi_reinstall_multi_env(pixi: Path, reinstall_workspace: Path) -> None
             manifest,
             "--environment",
             "dev",
-            "pixi-build-package",
+            "pixi_build_package_dev",
         ],
         env=env,
     )
-    assert not conda_metadata_params.is_file()
-    assert conda_build_params.is_file()
+    assert not conda_metadata_params_dev.is_file()
+    assert conda_build_params_dev.is_file()
     verify_cli_command(
         [
             pixi,
@@ -204,14 +234,14 @@ def test_pixi_reinstall_multi_env(pixi: Path, reinstall_workspace: Path) -> None
             manifest,
             "--environment",
             "dev",
-            "pixi-build-package-main",
+            "pixi-build-package-dev-main",
         ],
-        stdout_contains="Pixi Build is number 2",
+        stdout_contains="Pixi Build dev is number 2",
         env=env,
     )
     verify_cli_command(
-        [pixi, "run", "--manifest-path", manifest, "--environment", "dev", "pypi-package-main"],
-        stdout_contains="PyPI is number 1",
+        [pixi, "run", "--manifest-path", manifest, "--environment", "dev", "pypi-package-dev-main"],
+        stdout_contains="PyPI dev is number 1",
         env=env,
     )
 
@@ -225,14 +255,14 @@ def test_pixi_reinstall_multi_env(pixi: Path, reinstall_workspace: Path) -> None
             manifest,
             "--environment",
             "dev",
-            "pypi-package",
-            "pixi-build-package-main",
+            "pypi_package_dev",
+            "pixi_build_package_dev",
         ],
         env=env,
     )
     verify_cli_command(
-        [pixi, "run", "--manifest-path", manifest, "--environment", "dev", "pypi-package-main"],
-        stdout_contains="PyPI is number 2",
+        [pixi, "run", "--manifest-path", manifest, "--environment", "dev", "pypi-package-dev-main"],
+        stdout_contains="PyPI dev is number 2",
         env=env,
     )
     verify_cli_command(
@@ -243,9 +273,9 @@ def test_pixi_reinstall_multi_env(pixi: Path, reinstall_workspace: Path) -> None
             manifest,
             "--environment",
             "dev",
-            "pixi-build-package-main",
+            "pixi-build-package-dev-main",
         ],
-        stdout_contains="Pixi Build is number 2",
+        stdout_contains="Pixi Build dev is number 2",
         env=env,
     )
 
@@ -261,9 +291,15 @@ def test_pixi_reinstall_multi_env(pixi: Path, reinstall_workspace: Path) -> None
         env=env,
     )
 
+    # Delete the files to get a clean state
+    conda_metadata_params.unlink()
+    conda_build_params.unlink()
+
     # After reinstalling all environments,
     # also the default environment should be "number 2"
     verify_cli_command([pixi, "reinstall", "--manifest-path", manifest, "--all"], env=env)
+    assert not conda_metadata_params.is_file()
+    assert conda_build_params.is_file()
     verify_cli_command(
         [pixi, "run", "--manifest-path", manifest, "pypi-package-main"],
         stdout_contains="PyPI is number 2",
