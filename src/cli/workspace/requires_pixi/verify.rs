c9cc8f740e2a5c76373ccb0760693a0f418f72c1
James Z.M. Gao <gaozm55@gmail.com>
2025-03-17 09:36:31 +0800

    change exit code to 4 if verify pixi fail

--- src/cli/workspace/requires_pixi/verify.rs
+++ src/cli/workspace/requires_pixi/verify.rs
@@ -6,10 +6,20 @@ pub async fn execute(workspace: Workspace) -> miette::Result<()> {
     //   0: success
     //   1: failed to parse manifest
     //   2: failed to parse command line arguments
-    //   3: current pixi version is old
+    //   4: current pixi version is old
     match workspace.verify_current_pixi_meets_requirement() {
         Ok(_) => Ok(()),
-        Err(ExplicitManifestError::SelfVersionMatchError { .. }) => std::process::exit(3),
-        Err(e) => Err(e.into()),
+        Err(e) => {
+            if let ExplicitManifestError::SelfVersionMatchError { .. } = e {
+                eprintln!(
+                    "Error:   {}{}",
+                    console::style(console::Emoji("× ", "")).red(),
+                    e
+                );
+                std::process::exit(4);
+            } else {
+                Err(e.into())
+            }
+        }
     }
 }
