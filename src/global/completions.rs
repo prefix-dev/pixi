130e7bf851bcf9a057d34e6785b3db40a24eec0c
Julian Hofer <julianhofer@gnome.org>
2025-03-21 15:50:43 +0100

    Continue

--- src/global/completions.rs
+++ src/global/completions.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use indexmap::IndexSet;
 use rattler_shell::shell::{Bash, Fish, Shell, Zsh};
@@ -12,6 +12,7 @@ pub enum CompletionScript {
     Bash(PathBuf),
 }
 
+// TODO: use that function in the code
 pub fn find_completions(
     prefix: &Prefix,
     executables: &IndexSet<Mapping>,
@@ -25,35 +26,39 @@ pub fn find_completions(
             continue;
         }
 
-        let name = exe.executable_name();
-        let zsh_name = format!("_{name}");
-        let fish_name = format!("{name}.fish");
-
-        let bash_path = prefix
-            .root()
-            .join(Bash.completion_script_location().expect("is set"))
-            .join(name);
-        let zsh_path = prefix
-            .root()
-            .join(Zsh.completion_script_location().expect("is set"))
-            .join(zsh_name);
-        let fish_path = prefix
-            .root()
-            .join(Fish.completion_script_location().expect("is set"))
-            .join(fish_name);
-
-        if bash_path.exists() {
-            completion_scripts.push(CompletionScript::Bash(bash_path));
-        }
-
-        if zsh_path.exists() {
-            completion_scripts.push(CompletionScript::Zsh(zsh_path));
-        }
-
-        if fish_path.exists() {
-            completion_scripts.push(CompletionScript::Fish(fish_path));
-        }
+        completion_scripts.extend(contained_completions(prefix.root(), exe));
     }
 
     Ok(completion_scripts)
+}
+
+fn contained_completions(root: &Path, exe: &Mapping) -> Vec<CompletionScript> {
+    let mut completion_scripts = Vec::new();
+
+    let name = exe.executable_name();
+    let zsh_name = format!("_{name}");
+    let fish_name = format!("{name}.fish");
+
+    let bash_path = root
+        .join(Bash.completion_script_location().expect("is set"))
+        .join(name);
+    let zsh_path = root
+        .join(Zsh.completion_script_location().expect("is set"))
+        .join(zsh_name);
+    let fish_path = root
+        .join(Fish.completion_script_location().expect("is set"))
+        .join(fish_name);
+
+    if bash_path.exists() {
+        completion_scripts.push(CompletionScript::Bash(bash_path));
+    }
+
+    if zsh_path.exists() {
+        completion_scripts.push(CompletionScript::Zsh(zsh_path));
+    }
+
+    if fish_path.exists() {
+        completion_scripts.push(CompletionScript::Fish(fish_path));
+    }
+    completion_scripts
 }
