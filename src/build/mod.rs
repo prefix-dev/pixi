0305a3089a8804d2dd0e44d73c1d4ff041edd233
nichmor <nmorkotilo@gmail.com>
2025-03-28 08:46:01 +0200

    misc: remove eprintln

--- src/build/mod.rs
+++ src/build/mod.rs
@@ -734,7 +734,6 @@ impl BuildContext {
             })
             .await
             .map_err(|frontend_error| {
-                eprintln!("error setting up build frontend: {}", frontend_error);
                 tracing::error!("error setting up build frontend: {}", frontend_error);
                 BuildError::BuildFrontendSetup(Box::new(source.clone()), frontend_error)
             })?;
