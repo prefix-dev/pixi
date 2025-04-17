6c5d861b1afda7657d29e610e3c8f247bafb5132
Hofer-Julian <30049909+Hofer-Julian@users.noreply.github.com>
2025-04-17 15:40:53 +0200

    docs: rename `pixi build` examples (#3632)

--- docs/source_files/pixi_workspaces/pixi_build/workspace_variants/src/python_rich/__init__.py
+++ docs/source_files/pixi_workspaces/pixi_build/workspace_variants/src/python_rich/__init__.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, fields
 from rich.console import Console
 from rich.table import Table
-import python_bindings
+import cpp_math
 
 
 @dataclass
@@ -26,7 +26,7 @@ def main() -> None:
         table.add_column(column.name)
 
     for person in people:
-        updated_age = python_bindings.add(person.age, 1)
+        updated_age = cpp_math.add(person.age, 1)
         table.add_row(person.name, str(updated_age), person.city)
 
     console.print(table)
