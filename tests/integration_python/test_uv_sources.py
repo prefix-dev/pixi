164f3c9586d636ad4ef848f3591a26de2303facc
Tim de Jager <tim@prefix.dev>
2025-04-23 19:56:37 +0200

    fix: updated test and added all platforms for ci

--- tests/integration_python/test_uv_sources.py
+++ tests/integration_python/test_uv_sources.py
@@ -4,12 +4,27 @@ import shutil
 
 from .common import ExitCode, verify_cli_command
 
+
 @pytest.mark.slow
 def test_install_with_uv_sources(
-    pixi: Path, tmp_pixi_workspace: Path, test_data: Path,
-):
+    pixi: Path,
+    tmp_pixi_workspace: Path,
+    test_data: Path,
+) -> None:
     shutil.copytree(test_data / "uv-sources-non-root", tmp_pixi_workspace, dirs_exist_ok=True)
     verify_cli_command(
         [pixi, "install", "--manifest-path", tmp_pixi_workspace],
         expected_exit_code=ExitCode.SUCCESS,
     )
+    # import pdb; pdb.set_trace()
+    # Check if dist-info is available for local-library2
+    assert (
+        tmp_pixi_workspace
+        / ".pixi"
+        / "envs"
+        / "default"
+        / "lib"
+        / "python3.13"
+        / "site-packages"
+        / "local_library2-0.1.0.dist-info"
+    ).exists()
