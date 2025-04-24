ba619165b48231db4f6b8894e061732959c8636a
Tim de Jager <tim@prefix.dev>
2025-04-24 11:44:43 +0200

    fix: attempt to fix windows test

--- tests/integration_python/test_uv_sources.py
+++ tests/integration_python/test_uv_sources.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import pytest
 import shutil
+import sys
 
 from .common import ExitCode, verify_cli_command
 
@@ -17,13 +18,19 @@ def test_install_with_uv_sources(
         expected_exit_code=ExitCode.SUCCESS,
     )
     # Check if dist-info is available for local-library2
+    #
+    if sys.platform.startswith("win"):
+        python_dir = "python"
+    else:
+        python_dir = "python3.13"
+
     assert (
         tmp_pixi_workspace
         / ".pixi"
         / "envs"
         / "default"
         / "lib"
-        / "python3.13"
+        / python_dir
         / "site-packages"
         / "local_library2-0.1.0.dist-info"
     ).exists()
