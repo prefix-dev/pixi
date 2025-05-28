7f1ecf63b49674819dd7cbae0085684918b686fb
James Z.M. Gao <gaozm55@gmail.com>
2025-05-28 16:16:09 +0800

    fix: unify reqwest Client for self_update when downloading archives (#3346)
    Co-authored-by: Ruben Arts <ruben.arts@hotmail.com>

--- src/cli/self_update.rs
+++ src/cli/self_update.rs
@@ -4,9 +4,8 @@ use flate2::read::GzDecoder;
 use tar::Archive;
 
 use miette::IntoDiagnostic;
-use pixi_config::Config;
 use pixi_consts::consts;
-use reqwest::Client;
+use pixi_utils::reqwest::{build_reqwest_clients, reqwest_client_builder};
 use reqwest::redirect::Policy;
 
 use tempfile::{NamedTempFile, TempDir};
@@ -77,11 +76,10 @@ async fn latest_version() -> miette::Result<Version> {
     let url = format!("{}/latest", consts::RELEASES_URL);
 
     // Create a client with a redirect policy
-    let mut no_redirect_client_builder = Client::builder().redirect(Policy::none()); // Prevent automatic redirects
-    for p in Config::load_global().get_proxies().into_diagnostic()? {
-        no_redirect_client_builder = no_redirect_client_builder.proxy(p);
-    }
-    let no_redirect_client = no_redirect_client_builder.build().into_diagnostic()?;
+    let no_redirect_client = reqwest_client_builder(None)?
+        .redirect(Policy::none())
+        .build()
+        .into_diagnostic()?; // Prevent automatic redirects
 
     let version: String = match no_redirect_client
         .head(&url)
@@ -178,6 +176,7 @@ pub async fn execute(args: Args) -> miette::Result<()> {
         }
         None => latest_version().await?,
     };
+
     // Get the current version of the pixi binary
     let current_version = Version::from_str(consts::PIXI_VERSION).into_diagnostic()?;
 
@@ -264,8 +263,7 @@ pub async fn execute(args: Args) -> miette::Result<()> {
     // Create a temp file to download the archive
     let mut archived_tempfile = NamedTempFile::new().into_diagnostic()?;
 
-    // TODO proxy inject in https://github.com/prefix-dev/pixi/pull/3346
-    let client = Client::new();
+    let client = build_reqwest_clients(None, None)?.1;
     let mut res = client
         .get(&download_url)
         .header("User-Agent", user_agent())
