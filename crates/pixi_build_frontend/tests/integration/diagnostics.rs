4b4b774920819d0c7662eaa172781c25e7b0843b
Julian Hofer <julianhofer@gnome.org>
2025-06-12 15:57:49 +0200

    Fix replace_source_dir

--- crates/pixi_build_frontend/tests/integration/diagnostics.rs
+++ crates/pixi_build_frontend/tests/integration/diagnostics.rs
@@ -95,7 +95,7 @@ async fn test_invalid_manifest() {
 
 fn replace_source_dir(snapshot: &str, source_dir: &Path) -> String {
     snapshot.replace(
-        &(source_dir.display().to_string() + std::path::MAIN_SEPARATOR_STR),
+        &(source_dir.display().to_string().replace("\\", "/") + std::path::MAIN_SEPARATOR_STR),
         "[SOURCE_DIR]/",
     )
 }
