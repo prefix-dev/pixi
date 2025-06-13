b147e5536f8347419ccf46123b56b6d347b27b69
nichmor <nmorkotilo@gmail.com>
2025-06-13 17:09:08 +0300

    misc: add integration tests

--- tests/integration_rust/pypi_tests.rs
+++ tests/integration_rust/pypi_tests.rs
@@ -289,3 +289,80 @@ async fn test_allow_insecure_host() {
     .unwrap();
     pixi.update_lock_file().await.unwrap();
 }
+
+#[tokio::test]
+#[cfg_attr(not(feature = "slow_integration_tests"), ignore)]
+async fn test_indexes_are_passed_when_solving_build_pypi_dependencies() {
+    let pypi_indexes = Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/data/pypi-indexes");
+    let pypi_indexes_url = Url::from_directory_path(pypi_indexes.clone()).unwrap();
+
+    let pixi = PixiControl::from_pyproject_manifest(&format!(
+        r#"
+        [project]
+        name = "pypi-build-index"
+        requires-python = ">=3.10"
+        version = "0.1.0"
+
+        [build-system]
+        requires = [
+        "foo",
+        "hatchling",
+        ]
+        build-backend = "hatchling.build"
+
+        [tool.hatch.build]
+        include = ["src"]
+        targets.wheel.strict-naming = false
+        targets.wheel.packages = ["src/pypi_build_index"]
+        targets.sdist.strict-naming = false
+        targets.sdist.packages = ["src/pypi_build_index"]
+
+
+
+        [tool.pixi.project]
+        channels = ["https://prefix.dev/conda-forge"]
+        platforms = ["{platform}"]
+
+        [tool.pixi.dependencies]
+        hatchling = "*"
+
+        [tool.pixi.pypi-options]
+        index-url = "{pypi_indexes}multiple-indexes-a/index"
+        no-build-isolation = ["pypi-build-index"]
+
+        [tool.pixi.pypi-dependencies]
+        pypi-build-index = {{ path = ".", editable = true }}
+        "#,
+        platform = Platform::current(),
+        pypi_indexes = pypi_indexes_url,
+    ))
+    .unwrap();
+
+    let project_path = pixi.workspace_path();
+    let src_dir = project_path.join("src").join("pypi_build_index");
+    fs_err::create_dir_all(&src_dir).unwrap();
+    fs_err::write(src_dir.join("__init__.py"), "").unwrap();
+
+    let lock_file = pixi.update_lock_file().await.unwrap();
+
+    // verify that the pypi-build-index can be installed when solved the build dependencies
+    pixi.install().await.unwrap();
+
+    // verify that
+    assert_eq!(
+        lock_file
+            .default_environment()
+            .unwrap()
+            .pypi_indexes()
+            .unwrap()
+            .indexes
+            .first()
+            .unwrap()
+            .path(),
+        pypi_indexes
+            .join("multiple-indexes-a")
+            .join("index")
+            .display()
+            .to_string()
+    );
+}
