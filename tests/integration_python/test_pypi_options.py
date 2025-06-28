e220df0760ff3084b5dc39f63c4e303a86a24274
HernandoR <lzhen.dev@outlook.com>
2025-06-28 12:43:16 +0800

    draft: test(overide)
    Signed-off-by: HernandoR <lzhen.dev@outlook.com>

--- tests/integration_python/test_pypi_options.py
+++ tests/integration_python/test_pypi_options.py
@@ -21,3 +21,21 @@ def test_no_build_option(pixi: Path, tmp_pixi_workspace: Path, tmp_path: Path) -
         ExitCode.FAILURE,
         env={"PIXI_CACHE_DIR": str(tmp_path)},
     )
+
+
+def test_pypi_overrides(pixi: Path, tmp_pixi_workspace: Path, tmp_path: Path) -> None:
+    """
+    Tests the behavior of pixi install command with dependency overrides specified in pixi.toml.
+    This test verifies that the installation succeeds when the overrides are correctly defined.
+    """
+    test_data = Path(__file__).parent.parent / "data/pixi_tomls/depencency_overrides.toml"
+    manifest = tmp_pixi_workspace.joinpath("pixi.toml")
+    toml = test_data.read_text()
+    manifest.write_text(toml)
+
+    # Run the installation
+    verify_cli_command(
+        [pixi, "install", "--manifest-path", manifest],
+        ExitCode.SUCCESS,
+        env={"PIXI_CACHE_DIR": str(tmp_path)},
+    )
