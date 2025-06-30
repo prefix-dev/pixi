a9065984758fddcbeda8ee3ec6d54d6a71d54a80
Wolf Vollprecht <w.vollprecht@gmail.com>
2025-06-30 11:08:52 +0200

    fix hashes for python

--- tests/integration_rust/install_tests.rs
+++ tests/integration_rust/install_tests.rs
@@ -905,22 +905,34 @@ async fn test_multiple_prefix_update() {
 
     let project = pixi.workspace().unwrap();
 
-    let python_package = Package::build("python", "3.13.1").finish();
+    let (url, sha256, md5) = if cfg!(target_os = "windows") {
+        (
+            "https://repo.prefix.dev/conda-forge/win-64/python-3.13.1-h071d269_105_cp313.conda",
+            "de3bb832ff3982c993c6af15e6c45bb647159f25329caceed6f73fd4769c7628",
+            "3ddb0531ecfb2e7274d471203e053d78",
+        )
+    } else if cfg!(target_os = "macos") {
+        (
+            "https://repo.prefix.dev/conda-forge/osx-64/python-3.13.1-h2334245_105_cp313.conda",
+            "a9d224fa69c8b58c8112997f03988de569504c36ba619a08144c47512219e5ad",
+            "c3318c58d14fefd755852e989c991556",
+        )
+    } else {
+        (
+            "https://repo.prefix.dev/conda-forge/linux-64/python-3.13.1-ha99a958_105_cp313.conda",
+            "d3eb7d0820cf0189103bba1e60e242ffc15fd2f727640ac3a10394b27adf3cca",
+            "34945787453ee52a8f8271c1d19af1e8",
+        )
+    };
 
-    #[cfg(target_os = "windows")]
-    let package_url =
-        "https://repo.prefix.dev/conda-forge/win-64/python-3.13.1-h071d269_105_cp313.conda";
-    #[cfg(target_os = "linux")]
-    let package_url =
-        "https://repo.prefix.dev/conda-forge/linux-64/python-3.13.1-ha99a958_105_cp313.conda";
-    #[cfg(target_os = "macos")]
-    let package_url =
-        "https://repo.prefix.dev/conda-forge/osx-64/python-3.13.1-h2334245_105_cp313.conda";
+    let python_package = Package::build("python", "3.13.1")
+        .with_hashes(sha256, md5)
+        .finish();
 
     let python_repo_data_record = RepoDataRecord {
         package_record: python_package.package_record,
         file_name: "python".to_owned(),
-        url: Url::parse(package_url).unwrap(),
+        url: Url::parse(url).unwrap(),
         channel: Some("https://repo.prefix.dev/conda-forge/".to_owned()),
     };
 
