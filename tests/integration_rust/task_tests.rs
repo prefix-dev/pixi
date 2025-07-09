14713875999b75858ed260b3f27997878540a509
Magenta <magenta2127@gmail.com>
2025-07-09 20:43:59 +0200

    fix: fix escaping issues

--- tests/integration_rust/task_tests.rs
+++ tests/integration_rust/task_tests.rs
@@ -246,7 +246,7 @@ async fn test_task_with_env() {
     // Windows equires that %VAR% is defined in its environment
     // that is: if the parent sets it but the child shell does not inherit it, %VAR% is empty.
     let echo_cmd: Vec<&'static str> = if cfg!(windows) {
-        vec!["powershell", "-Command", "echo From a $Env:HELLO_WORLD"]
+        vec!["powershell", "-Command", "echo \"From a $Env:HELLO_WORLD\""]
     } else {
         vec!["sh", "-c", "echo 'From a $HELLO_WORLD'"]
     };
