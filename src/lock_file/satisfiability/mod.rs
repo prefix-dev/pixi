4901470ba5fd192ed9a19ad71c5a4d78a6bd6ccb
Ruben Arts <ruben.arts@hotmail.com>
2025-07-22 09:48:29 +0200

    test: add the build examples to some of our tests

--- src/lock_file/satisfiability/mod.rs
+++ src/lock_file/satisfiability/mod.rs
@@ -1929,8 +1929,8 @@ mod tests {
 
     #[rstest]
     #[tokio::test]
-    #[cfg_attr(not(feature = "slow_integration_tests"), ignore)]
-    async fn test_example_satisfiability(#[files("examples/*/p*.toml")] manifest_path: PathBuf) {
+    // #[cfg_attr(not(feature = "slow_integration_tests"), ignore)]
+    async fn test_example_satisfiability(#[files("examples/**/p*.toml")] manifest_path: PathBuf) {
         // If a pyproject.toml is present check for `tool.pixi` in the file to avoid
         // testing of non-pixi files
         if manifest_path.file_name().unwrap() == "pyproject.toml" {
