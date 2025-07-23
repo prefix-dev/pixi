addb52b5c0940db800796cd5154a057d869114aa
Magenta <magenta2127@gmail.com>
2025-07-23 13:34:22 +0200

    feat: replace upload with rattler_upload

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -36,8 +36,9 @@ pub mod task;
 pub mod tree;
 pub mod update;
 pub mod upgrade;
-pub mod upload;
 pub mod workspace;
+use rattler_upload::upload::opt::UploadOpts;
+use rattler_upload::upload_from_args;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -163,7 +164,7 @@ pub enum Command {
     Tree(tree::Args),
     Update(update::Args),
     Upgrade(upgrade::Args),
-    Upload(upload::Args),
+    Upload(UploadOpts),
     #[clap(alias = "project")]
     Workspace(workspace::Args),
     #[command(external_subcommand)]
@@ -399,7 +400,7 @@ pub async fn execute_command(
         Command::Task(cmd) => task::execute(cmd).await,
         Command::Info(cmd) => info::execute(cmd).await,
         Command::Import(cmd) => import::execute(cmd).await,
-        Command::Upload(cmd) => upload::execute(cmd).await,
+        Command::Upload(upload_args) => upload_from_args(upload_args).await,
         Command::Search(cmd) => search::execute(cmd).await,
         Command::Workspace(cmd) => workspace::execute(cmd).await,
         Command::Remove(cmd) => remove::execute(cmd).await,
