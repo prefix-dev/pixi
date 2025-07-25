199d7822fc6311f826080a9c2251abc8edfafc08
Julian Hofer <julianhofer@gnome.org>
2025-07-25 09:52:29 +0200

    Small test improvement

--- tests/integration_python/test_environment_variables.py
+++ tests/integration_python/test_environment_variables.py
@@ -16,11 +16,12 @@ def test_variable_expansion(pixi: Path, tmp_pixi_workspace: Path) -> None:
         TEST_VAR = "$PIXI_PROJECT_NAME"
 
         [tasks]
-        start = "echo $TEST_VAR"
+        start = "echo The project name is $TEST_VAR"
         """
     manifest.write_text(toml)
 
     # If variable expansion works, we expect `$PIXI_PROJECT_NAME` to expand to the project name "expansion-test"
     verify_cli_command(
-        [pixi, "run", "--manifest-path", manifest, "start"], stdout_contains="expansion-test"
+        [pixi, "run", "--manifest-path", manifest, "start"],
+        stdout_contains="The project name is expansion-test",
     )
