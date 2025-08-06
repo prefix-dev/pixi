d79cfc2d973f2bb0ecbb0964eec62521d88de162
magentaqin <magenta2127@gmail.com>
2025-08-06 10:12:35 +0200

    chore: turn comment to docstring

--- tests/integration_python/test_run_cli.py
+++ tests/integration_python/test_run_cli.py
@@ -1600,8 +1600,8 @@ def test_signal_forwarding(pixi: Path, tmp_pixi_workspace: Path) -> None:
     else:
         raise AssertionError("Output file was not created")
     
-# Test environment variables should not be overridden by excluded keys.
-def test_run_environment_variable_not_be_overridden(pixi: Path, tmp_pixi_workspace: Path) -> None: 
+def test_run_environment_variable_not_be_overridden(pixi: Path, tmp_pixi_workspace: Path) -> None:
+    """Test environment variables should not be overridden by excluded keys.""" 
     manifest = tmp_pixi_workspace.joinpath("pixi.toml")
     toml = """
     [workspace]
