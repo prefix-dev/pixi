e618914c9f10d32b058057e6017523618051e20e
Lucas Colley <lucas.colley8@gmail.com>
2025-09-12 21:39:08 +0100

    Revert "chore(add): clear error for missing channel in url (#4538)"
    This reverts commit 8126861f34484dc2c512bf0d5bb06190ff0cc096.

--- crates/pixi/tests/integration_rust/common/mod.rs
+++ crates/pixi/tests/integration_rust/common/mod.rs
@@ -432,7 +432,7 @@ impl PixiControl {
         }
     }
 
-    /// Remove a channel from the project.
+    /// Add a new channel to the project.
     pub fn project_channel_remove(&self) -> ProjectChannelRemoveBuilder {
         ProjectChannelRemoveBuilder {
             manifest_path: Some(self.manifest_path()),
