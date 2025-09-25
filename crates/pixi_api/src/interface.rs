66aa61179562c05e831457aff4a291bb5bb85210
Felix Häcker <felix@prefix.dev>
2025-09-25 15:38:26 +0200

    Drop styled text abstraction

--- crates/pixi_api/src/interface.rs
+++ crates/pixi_api/src/interface.rs
@@ -1,10 +1,7 @@
 use miette::Result;
 use std::future::Future;
 
-use crate::styled_text::StyledText;
-
 pub trait Interface {
-    fn styled(&self, text: StyledText) -> String;
     fn is_cli(&self) -> impl Future<Output = bool> + Send;
     fn confirm(&self, msg: &str) -> impl Future<Output = Result<bool>> + Send;
     fn info(&self, msg: &str) -> impl Future<Output = ()> + Send;
