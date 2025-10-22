5a2fd5d62622578a36bacf3570204996e6835cc3
Julian Hofer <julianhofer@gnome.org>
2025-10-22 11:08:40 +0200

    chore: version to 0.58.0

--- tests/integration_python/common.py
+++ tests/integration_python/common.py
@@ -9,7 +9,7 @@ from typing import override
 
 from rattler import Platform
 
-PIXI_VERSION = "0.57.0"
+PIXI_VERSION = "0.58.0"
 
 
 ALL_PLATFORMS = '["linux-64", "osx-64", "osx-arm64", "win-64", "linux-ppc64le", "linux-aarch64"]'
