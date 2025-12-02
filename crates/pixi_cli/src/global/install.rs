61af438cdc879c35a2d3453c78793b14cf4275d9
Julian Hofer <julianhofer@gnome.org>
2025-12-02 10:11:03 +0100

    Remove unneeded comment

--- crates/pixi_cli/src/global/install.rs
+++ crates/pixi_cli/src/global/install.rs
@@ -124,7 +124,6 @@ pub async fn execute(args: Args) -> miette::Result<()> {
     let mut env_changes = EnvChanges::default();
     let mut last_updated_project = project_original;
     let mut errors: Vec<(EnvironmentName, Report)> = Vec::new();
-    // Convert the packages into named global specs
 
     for (env_name, specs) in &env_to_specs {
         let mut project = last_updated_project.clone();
