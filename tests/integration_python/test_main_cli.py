b130eaa23ed19390301bd2b801e6ab51fadb779c
prady0t <rickprimeranjan@gmail.com>
2025-12-08 02:15:31 +0530

    Removing redundent tests
    Signed-off-by: prady0t <rickprimeranjan@gmail.com>

--- tests/integration_python/test_main_cli.py
+++ tests/integration_python/test_main_cli.py
@@ -1415,5 +1415,5 @@ def test_add_url_no_channel(pixi: Path, tmp_pixi_workspace: Path) -> None:
             "--manifest-path",
             tmp_pixi_workspace,
         ],
-        stderr_contains="Added https://repo.prefix.dev/bioconda::libzlib",
+        stderr_contains="Added libzlib",
     )
