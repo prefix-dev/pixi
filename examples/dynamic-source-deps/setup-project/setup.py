612e7a49c5db4911ff584759a5b47a2fc009f8d4
nichmor <nmorkotilo@gmail.com>
2026-01-06 23:35:30 +0200

    misc: add dynamic-source-deps example

---
+++ examples/dynamic-source-deps/setup-project/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+def get_version():
+    return "0.5.0"
+
+
+setup(
+    name="setup_project",
+    version=get_version(),
+    description="A package with setup.py for dynamic metadata testing",
+    packages=["setup_project"],
+)
