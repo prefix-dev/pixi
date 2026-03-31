b7e13c881942eff25ddb5c7f5fd3097238889863
Pavel Zwerschke <pavelzw@gmail.com>
2026-03-31 23:59:33 +0200

    WIP

--- crates/pixi_cli/src/publish.rs
+++ crates/pixi_cli/src/publish.rs
@@ -195,7 +195,7 @@ pub async fn execute(args: Args) -> miette::Result<()> {
         preferred_build_source: None,
         channels: channels.clone(),
         channel_config: channel_config.clone(),
-        // When running `pixi build`, the exclude_newer config will be ignored.
+        // When running `pixi publish`, the exclude_newer config will be ignored.
         // It will only be used when using the package as a source depdendency.
         exclude_newer: None,
         build_environment: build_environment.clone(),
@@ -236,7 +236,7 @@ pub async fn execute(args: Args) -> miette::Result<()> {
                 output_directory: None,
                 source: PinnedSourceCodeLocation::new(manifest_source.clone(), None),
                 channels: channels.clone(),
-                // When running `pixi build`, the exclude_newer config will be ignored.
+                // When running `pixi publish`, the exclude_newer config will be ignored.
                 // It will only be used when using the package as a source depdendency.
                 exclude_newer: None,
                 channel_config: channel_config.clone(),
