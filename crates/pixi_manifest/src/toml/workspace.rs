1aba6c71fb5327bfacf63ec34f53be9e1ecbc369
Pavel Zwerschke <pavelzw@gmail.com>
2026-04-02 01:45:00 +0200

    WIP

--- crates/pixi_manifest/src/toml/workspace.rs
+++ crates/pixi_manifest/src/toml/workspace.rs
@@ -392,7 +392,7 @@ mod test {
             .and_then(|w| w.into_workspace(ExternalWorkspaceProperties::default(), Some(path)))
             .unwrap_err();
         assert_snapshot!(format_parse_error(input, parse_error), @r#"
-         × `date` is neither a valid duration nor timestamp (premature end of input)
+         × `date` is neither a valid duration, date (input contains invalid characters), nor timestamp (premature end of input)
           ╭─[pixi.toml:4:26]
         3 │         platforms = []
         4 │         exclude-newer = "date"
