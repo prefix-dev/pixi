3ab727e91e42eb896a792db10fc4d72eafa519e8
lisiqi1983 <169274571+lisiqi1983@users.noreply.github.com>
2026-05-13 09:14:47 +0800

    Allow unsetting unknown config keys

--- crates/pixi_config/src/lib.rs
+++ crates/pixi_config/src/lib.rs
@@ -2132,13 +2132,18 @@ impl Config {
     ///
     /// It is required to call `save()` to persist the changes.
     pub fn set(&mut self, key: &str, value: Option<String>) -> miette::Result<()> {
-        let show_supported_keys =
-            || format!("Supported keys:\n\t{}", self.get_keys().join(",\n\t"));
-        let err = miette::miette!(
-            "Unknown key: {}\n{}",
-            console::style(key).red(),
-            show_supported_keys()
-        );
+        let supported_keys = format!("Supported keys:\n\t{}", self.get_keys().join(",\n\t"));
+        let unknown_key = || {
+            if value.is_none() {
+                Ok(())
+            } else {
+                Err(miette::miette!(
+                    "Unknown key: {}\n{}",
+                    console::style(key).red(),
+                    supported_keys
+                ))
+            }
+        };
 
         match key {
             "default-channels" => {
@@ -2210,7 +2215,7 @@ impl Config {
                         .unwrap_or_default();
                     return Ok(());
                 } else if !key.starts_with("repodata-config.") {
-                    return Err(err);
+                    return unknown_key();
                 }
 
                 let subkey = key.strip_prefix("repodata-config.").unwrap();
@@ -2227,7 +2232,7 @@ impl Config {
                         self.repodata_config.default.disable_sharded =
                             value.map(|v| v.parse()).transpose().into_diagnostic()?;
                     }
-                    _ => return Err(err),
+                    _ => return unknown_key(),
                 }
             }
             key if key.starts_with("pypi-config") => {
@@ -2239,7 +2244,7 @@ impl Config {
                     }
                     return Ok(());
                 } else if !key.starts_with("pypi-config.") {
-                    return Err(err);
+                    return unknown_key();
                 }
 
                 let subkey = key.strip_prefix("pypi-config.").unwrap();
@@ -2273,7 +2278,7 @@ impl Config {
                             .into_diagnostic()?
                             .unwrap_or_default();
                     }
-                    _ => return Err(err),
+                    _ => return unknown_key(),
                 }
             }
             key if key.starts_with("s3-options") => {
@@ -2286,7 +2291,7 @@ impl Config {
                     return Ok(());
                 }
                 let Some(subkey) = key.strip_prefix("s3-options.") else {
-                    return Err(err);
+                    return unknown_key();
                 };
                 if let Some((bucket, rest)) = subkey.split_once('.') {
                     if let Some(bucket_config) = self.s3_options.get_mut(bucket) {
@@ -2323,7 +2328,7 @@ impl Config {
                                     ));
                                 }
                             }
-                            _ => return Err(err),
+                            _ => return unknown_key(),
                         }
                     }
                 } else {
@@ -2342,7 +2347,7 @@ impl Config {
                     }
                     return Ok(());
                 } else if !key.starts_with(format!("{EXPERIMENTAL}.").as_str()) {
-                    return Err(err);
+                    return unknown_key();
                 }
 
                 let subkey = key
@@ -2353,7 +2358,7 @@ impl Config {
                         self.experimental.use_environment_activation_cache =
                             value.map(|v| v.parse()).transpose().into_diagnostic()?;
                     }
-                    _ => return Err(err),
+                    _ => return unknown_key(),
                 }
             }
             key if key.starts_with("concurrency") => {
@@ -2365,7 +2370,7 @@ impl Config {
                     }
                     return Ok(());
                 } else if !key.starts_with("concurrency.") {
-                    return Err(err);
+                    return unknown_key();
                 }
                 let subkey = key.strip_prefix("concurrency.").unwrap();
                 match subkey {
@@ -2383,7 +2388,7 @@ impl Config {
                             return Err(miette!("'downloads' requires a number value"));
                         }
                     }
-                    _ => return Err(err),
+                    _ => return unknown_key(),
                 }
             }
             key if key.starts_with("shell") => {
@@ -2395,7 +2400,7 @@ impl Config {
                     }
                     return Ok(());
                 } else if !key.starts_with("shell.") {
-                    return Err(err);
+                    return unknown_key();
                 }
                 let subkey = key.strip_prefix("shell.").unwrap();
                 match subkey {
@@ -2411,7 +2416,7 @@ impl Config {
                         self.shell.change_ps1 =
                             value.map(|v| v.parse()).transpose().into_diagnostic()?;
                     }
-                    _ => return Err(err),
+                    _ => return unknown_key(),
                 }
             }
             key if key.starts_with("run-post-link-scripts") => {
@@ -2444,7 +2449,7 @@ impl Config {
                     }
                     return Ok(());
                 } else if !key.starts_with("proxy-config.") {
-                    return Err(err);
+                    return unknown_key();
                 }
 
                 let subkey = key.strip_prefix("proxy-config.").unwrap();
@@ -2468,7 +2473,7 @@ impl Config {
                             .into_diagnostic()?
                             .unwrap_or_default();
                     }
-                    _ => return Err(err),
+                    _ => return unknown_key(),
                 }
             }
             key if key.starts_with("cache") => {
@@ -2482,7 +2487,7 @@ impl Config {
                     self.cache.validate()?;
                     return Ok(());
                 } else if !key.starts_with("cache.") {
-                    return Err(err);
+                    return unknown_key();
                 }
                 let subkey = key.strip_prefix("cache.").unwrap();
                 match subkey {
@@ -2505,12 +2510,12 @@ impl Config {
                             .into_diagnostic()?
                             .unwrap_or_default();
                     }
-                    _ => return Err(err),
+                    _ => return unknown_key(),
                 }
                 self.cache.expand_paths()?;
                 self.cache.validate()?;
             }
-            _ => return Err(err),
+            _ => return unknown_key(),
         }
 
         Ok(())
@@ -3404,7 +3409,11 @@ UNUSED = "unused"
             .unwrap();
         assert_eq!(config.pinning_strategy, Some(PinningStrategy::Semver));
 
-        config.set("unknown-key", None).unwrap_err();
+        config
+            .set("unknown-key", Some("value".to_string()))
+            .unwrap_err();
+        config.set("unknown-key", None).unwrap();
+        config.set("repodata-config.disable-jlap", None).unwrap();
     }
 
     #[rstest]
