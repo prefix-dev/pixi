5eba2e7f3d25386c2e979cd8f8ed13df36509175
aoright <102943475+aoright@users.noreply.github.com>
2026-09-11 16:16:46 +0800

    fix: concurrency config set correctly targets concurrency config

--- crates/pixi_config/src/lib.rs
+++ crates/pixi_config/src/lib.rs
@@ -2508,9 +2508,9 @@ impl Config {
             key if key.starts_with("concurrency") => {
                 if key == "concurrency" {
                     if let Some(value) = value {
-                        self.pypi_config = serde_json::de::from_str(&value).into_diagnostic()?;
+                        self.concurrency = serde_json::de::from_str(&value).into_diagnostic()?;
                     } else {
-                        self.pypi_config = PyPIConfig::default();
+                        self.concurrency = ConcurrencyConfig::default();
                     }
                     return Ok(());
                 } else if !key.starts_with("concurrency.") {
